77716701cca686be15fa4f76c205c100bcf2d08d
Sven Augustin <sven.augustin@psi.ch>
2020-03-12 13:40:28 +0000

    some naming in DBCounter arguments

--- slic/daq/dbcounter.py
+++ slic/daq/dbcounter.py
@@ -11,16 +11,16 @@ def dapi_to_h5(filename, channels, **kwargs):
     data.to_hdf(filename, "/data")
 
 
-def dapi_get(channels, start_time_delta=None, end_time_delta=None):
-    start_time_delta = start_time_delta if start_time_delta is not None else dict(seconds=0)
-    end_time_delta   = end_time_delta   if end_time_delta   is not None else dict(seconds=1)
+def dapi_get(channels, start=None, end=None):
+    start = start if start is not None else dict(seconds=0)
+    end   = end   if end   is not None else dict(seconds=1)
 
-    start_time_delta = timedelta(**start_time_delta)
-    end_time_delta   = timedelta(**end_time_delta)
+    start_time_delta = timedelta(**start)
+    end_time_delta   = timedelta(**end)
 
     now   = datetime.now()
-    end   = now - end_time_delta
-    start = end - start_time_delta
+    start = now + start_time_delta
+    end   = start + end_time_delta
 
     return dapi.get_data(channels=channels, start=start, end=end)
 
