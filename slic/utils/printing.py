b42bd1f0566243c3a71bc042039da39a579af6b3
Sven Augustin <sven.augustin@psi.ch>
2020-03-21 16:51:09 +0000

    printable dict may have header, added printable dict of dicts

--- slic/utils/printing.py
+++ slic/utils/printing.py
@@ -1,19 +1,29 @@
 
-def format_header(msg):
-    msg += ":"
-    line = "-" * len(msg)
-    msg += "\n" + line
-    return msg
+def printable_dict_of_dicts(d):
+    lines = (printable_dict(v, header=k) for k, v in d.items())
+    return "\n".join(lines)
 
 
-def printable_dict(d):
+def printable_dict(d, header=None):
     length = maxlen(d) + 1
     lines = sorted("{}:{}{}".format(k, " "*(length-len(k)), v) for k, v in d.items())
-    return "\n".join(lines)
+
+    if header:
+        header = format_header(header)
+        lines = [header] + lines
+
+    return "\n".join(lines) + "\n"
 
 
 def maxlen(seq):
     return max(len(i) for i in seq)
+
+
+def format_header(msg, line_char="-"):
+    msg += ":"
+    line = line_char * len(msg)
+    msg += "\n" + line
+    return msg
 
 
 
