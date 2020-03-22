bf85e933107e9ea4074609f08304dcbf745fc8a9
Sven Augustin <sven.augustin@psi.ch>
2020-03-22 13:11:29 +0000

    added instrument and pgroup to bsread/dapi counters to generate folders, added instrument to config json

--- slic/daq/counter.py
+++ slic/daq/counter.py
@@ -1,18 +1,24 @@
 import os
 from abc import abstractmethod
 
-from .basecounter import BaseCounter
-from .utils import can_create_file, fix_hdf5_filename
-
+from ..utils.channels import Channels
 from .acquisition import Acquisition
+from .basecounter import BaseCounter
+from .utils import can_create_file, fix_hdf5_filename, SwissFELPaths
 
 
 
 class Counter(BaseCounter):
 
-    def __init__(self, default_channels=None, default_path="."):
-        self.default_channels = default_channels
-        self.default_path = default_path
+    def __init__(self, instrument, pgroup, default_channels=None, default_path=None):
+        paths = SwissFELPaths(instrument, pgroup)
+
+        if not default_channels:
+            default_channel_list = paths.default_channel_list
+            default_channels = Channels(default_channel_list)
+
+        if not default_path:
+            default_path = paths.raw
 
 
     def acquire(self, filename=None, channels=None, use_default_path=True, **kwargs):
