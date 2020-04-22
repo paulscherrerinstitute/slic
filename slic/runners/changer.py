c4a0743e599794fa7b946f6c077fd72d9ddf7de3
Sven Augustin <sven.augustin@psi.ch>
2020-04-22 08:10:03 +0000

    cleaned up Changer

--- slic/runners/changer.py
+++ slic/runners/changer.py
@@ -2,27 +2,37 @@ from threading import Thread
 
 
 class Changer:
-    def __init__(self, target=None, parent=None, changer=None, hold=True, stopper=None):
-        self.target = target
-        self._changer = changer
-        self._stopper = stopper
-        self._thread = Thread(target=self._changer,args=(target,))
-        if not hold:
-            self._thread.start()
 
-    def wait(self):
-        self._thread.join()
+    def __init__(self, target, changer, stopper=None, hold=True):
+        self.target = target
+        self.changer = changer
+        self.stopper = stopper
+        self.thread = Thread(target=changer, args=(target,))
+        if not hold:
+            self.thread.start()
 
     def start(self):
-        self._thread.start()
+        self.thread.start()
 
-    def status(self):
-        if self._thread.ident is None:
-            return 'waiting'
-        else:
-            if self._thread.isAlive:
-                return 'changing'
-            else:
-                return 'done'
     def stop(self):
-        self._stopper()
+        if self.stopper is not None:
+            self.stopper()
+
+    def wait(self):
+        self.thread.join()
+
+    @property
+    def status(self):
+        if self.thread.ident is None:
+            return "ready"
+        else:
+            if self.thread.isAlive():
+                return "changing"
+            else:
+                return "done"
+
+    def __repr__(self):
+        return "Changer: {}".format(self.status)
+
+
+
