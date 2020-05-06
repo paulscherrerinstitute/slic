8e416d3f502057da8b7801516b02902f48e91f0b
Sven Augustin <sven.augustin@psi.ch>
2020-05-06 09:36:12 +0000

    renamed utils.ipy_tools to utils.ipy

--- slic/utils/__init__.py
+++ slic/utils/__init__.py
@@ -4,7 +4,7 @@ from .channels import Channels
 from .config import Config
 from .elog import Elog
 from .path import can_create_file, glob_files, make_missing_dir
-from .ipy_tools import devices
+from .ipy import devices
 from .json_tools import json_dump
 from .npy_tools import fraction_to_percentage, within, within_fraction, get_dtype, get_shape, is_array
 from .screenshot import Screenshot
