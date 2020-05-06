2a236f32e74cbc41def87cb4ab48e7b123e67c1a
Sven Augustin <sven.augustin@psi.ch>
2020-05-06 09:55:06 +0000

    added json_load to slic.utils

--- slic/utils/__init__.py
+++ slic/utils/__init__.py
@@ -4,7 +4,7 @@ from .channels import Channels
 from .config import Config
 from .elog import Elog
 from .ipy import devices
-from .json import json_save
+from .json import json_save, json_load
 from .npy import fraction_to_percentage, within, within_fraction, get_dtype, get_shape, is_array
 from .path import can_create_file, glob_files, make_missing_dir
 from .screenshot import Screenshot
