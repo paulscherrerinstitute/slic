a1aded7db820293da326ddbf7cf62873dab8e35c
Sven Augustin <sven.augustin@psi.ch>
2020-05-06 09:36:58 +0000

    renamed utils.npy_tools to utils.npy

--- slic/utils/__init__.py
+++ slic/utils/__init__.py
@@ -6,7 +6,7 @@ from .elog import Elog
 from .path import can_create_file, glob_files, make_missing_dir
 from .ipy import devices
 from .json_tools import json_dump
-from .npy_tools import fraction_to_percentage, within, within_fraction, get_dtype, get_shape, is_array
+from .npy import fraction_to_percentage, within, within_fraction, get_dtype, get_shape, is_array
 from .screenshot import Screenshot
 
 
