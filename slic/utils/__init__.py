2ffd1b55cedef11d532474c1e4b3078d8e6f18e4
Sven Augustin <sven.augustin@psi.ch>
2020-05-06 09:35:27 +0000

    renamed utils.fs_tools to utils.path

--- slic/utils/__init__.py
+++ slic/utils/__init__.py
@@ -3,7 +3,7 @@ from .utils import *
 from .channels import Channels
 from .config import Config
 from .elog import Elog
-from .fs_tools import can_create_file, glob_files, make_missing_dir
+from .path import can_create_file, glob_files, make_missing_dir
 from .ipy_tools import devices
 from .json_tools import json_dump
 from .npy_tools import fraction_to_percentage, within, within_fraction, get_dtype, get_shape, is_array
