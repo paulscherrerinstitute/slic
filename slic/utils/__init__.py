f8951a9e4c8eb4b84ffa64a4af1e8c52a00a6d0e
Sven Augustin <sven.augustin@psi.ch>
2020-05-06 09:53:55 +0000

    imports ordered alphabetically

--- slic/utils/__init__.py
+++ slic/utils/__init__.py
@@ -3,10 +3,10 @@ from .utils import *
 from .channels import Channels
 from .config import Config
 from .elog import Elog
-from .path import can_create_file, glob_files, make_missing_dir
 from .ipy import devices
 from .json import json_save
 from .npy import fraction_to_percentage, within, within_fraction, get_dtype, get_shape, is_array
+from .path import can_create_file, glob_files, make_missing_dir
 from .screenshot import Screenshot
 
 
