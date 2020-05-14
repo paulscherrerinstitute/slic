b251820eff14dffa418c9c5310546d8ebde4b622
Sven Augustin <sven.augustin@psi.ch>
2020-05-14 16:03:46 +0000

    only use extension in pattern for matching; renamed extension to suffix, added _scan_info. to it; flipped run_index and name in _fill_filename_pattern

--- slic/core/scanner/runname.py
+++ slic/core/scanner/runname.py
@@ -9,18 +9,18 @@ DIGITS = "[0-9]"
 
 class RunFilenameGenerator:
 
-    def __init__(self, base_dir, prefix="run", n_digits=4, separator="_", extension="json"):
+    def __init__(self, base_dir, prefix="run", n_digits=4, separator="_", suffix="_scan_info.json"):
         self.base_dir = base_dir
         self.prefix = prefix
         self.n_digits = n_digits
         self.separator = separator
-        self.extension = extension
+        self.suffix = suffix
 
     def get_next_run_filename(self, name):
         runnums = self.get_existing_runnumbers()
         irun = next_int(runnums)
         formatted_irun = zero_pad(irun, self.n_digits)
-        return self._fill_filename_pattern(name, formatted_irun)
+        return self._fill_filename_pattern(formatted_irun, name)
 
     def get_existing_runnumbers(self):
         fnames = glob_files(self.base_dir, self.pattern)
@@ -30,10 +30,12 @@ class RunFilenameGenerator:
     @property
     def pattern(self):
         runnum_pattern = self.n_digits * DIGITS
-        return self._fill_filename_pattern(EVERYTHING, runnum_pattern)
+        res = self._fill_filename_pattern(runnum_pattern, EVERYTHING)
+        res += self.suffix
+        return res
 
-    def _fill_filename_pattern(self, name, run_index):
-        return self.prefix + run_index + self.separator + name + "." + self.extension
+    def _fill_filename_pattern(self, run_index, name):
+        return self.prefix + run_index + self.separator + name
 
 
 
