3fb3572196c4843e2abcf402cd80ffda35709406
Sven Augustin <sven.augustin@psi.ch>
2020-05-15 11:25:37 +0000

    added prototype for a lazy-connecting PV

---
+++ slic/utils/lazypv.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import epics
+
+
+class PV:
+
+    def __init__(self, *args, **kwargs):
+        self._args = args
+        self._kwargs = kwargs
+        self._pv = None
+
+    def __getattr__(self, name):
+        if self._pv is None:
+            args = self._args
+            kwargs = self._kwargs
+            self._pv = epics.PV(*args, **kwargs)
+        return getattr(self._pv, name)
+
+
+
