3469a38d58248ebf9af78d5093f3a8af5f6059c6
Sven Augustin <sven.augustin@psi.ch>
2020-06-04 18:51:21 +0200

    added files for packaging: setuptools, travis, conda

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="slic",
+    version="0.0.1",
+    url="https://gitlab.psi.ch/slic/slic",
+    description="SwissFEL Library for Instrument Control",
+    author="Paul Scherrer Institute",
+    packages=find_packages()
+)
+
+
