fa115981dde775a73519f2a8f8f76af99c62581a
Sven Augustin <sven.augustin@psi.ch>
2020-07-15 16:15:21 +0200

    first clean up

--- slic/devices/general/adjustable.py
+++ slic/devices/general/adjustable.py
@@ -4,20 +4,12 @@ from slic.utils.eco_components.aliases import Alias
 from enum import IntEnum, auto
 import colorama
 import time
-import logging
 import datetime
 
 
-logger = logging.getLogger(__name__)
 
-
-# exceptions
-class AdjustableError(Exception):
-    pass
-
-
-# wrappers for adjustables >>>>>>>>>>>
 def default_representation(Obj):
+
     def get_name(Obj):
         if Obj.alias:
             return Obj.alias.get_full_name()
@@ -36,8 +28,9 @@ def default_representation(Obj):
     return Obj
 
 
+
 def spec_convenience(Adj):
-    # spec-inspired convenience methods
+
     def mv(self, value):
         self._currentChange = self.set_target_value(value)
         return self._currentChange
@@ -87,142 +80,9 @@ def spec_convenience(Adj):
     return Adj
 
 
-class ValueInRange:
-    def __init__(self, start_value, end_value, bar_width=30, unit="", fmt="1.5g"):
-        self.start_value = start_value
-        self.end_value = end_value
-        self.unit = unit
-        self.bar_width = bar_width
-        self._blocks = " ▏▎▍▌▋▊▉█"
-        self._fmt = fmt
-
-    def get_str(self, value):
-        if self.start_value == self.end_value:
-            frac = 1
-        else:
-            frac = (value - self.start_value) / (self.end_value - self.start_value)
-        return (
-            f"{self.start_value:{self._fmt}}"
-            + self.get_unit_str()
-            + "|"
-            + self.bar_str(frac)
-            + "|"
-            + f"{self.end_value:{self._fmt}}"
-            + self.get_unit_str()
-        )
-
-    def get_unit_str(self):
-        if not self.unit:
-            return ""
-        else:
-            return " " + self.unit
-
-    def bar_str(self, frac):
-        blocks = self._blocks
-        if 0 < frac and frac <= 1:
-            whole = int(self.bar_width // (1 / frac))
-            part = int((frac * self.bar_width - whole) // (1 / (len(blocks) - 1)))
-            return (
-                colorama.Fore.GREEN
-                + whole * blocks[-1]
-                + blocks[part]
-                + (self.bar_width - whole - 1) * blocks[0]
-                + colorama.Fore.RESET
-            )
-        elif frac == 0:
-            return self.bar_width * blocks[0]
-        elif frac < 0:
-            return colorama.Fore.RED + "<" * self.bar_width + colorama.Fore.RESET
-        elif frac > 1:
-            return colorama.Fore.RED + ">" * self.bar_width + colorama.Fore.RESET
-
-
-def update_changes(Adj):
-    def get_position_str(start, end, value):
-        s = ValueInRange(start, end, bar_width=30, unit="", fmt="1.5g").get_str(value)
-        return (
-            colorama.Style.BRIGHT
-            + f"{value:1.5}".rjust(10)
-            + colorama.Style.RESET_ALL
-            + "  "
-            + s
-            + 2 * "\t"
-        )
-
-    def update_change(self, value):
-        start = self.get_current_value()
-        print(
-            f"Changing {self.name} from {start:1.5g} by {value-start:1.5g} to {value:1.5g}\n"
-        )
-        print(get_position_str(start, value, start), end="\r")
-        try:
-
-            def cbfoo(**kwargs):
-                print(get_position_str(start, value, kwargs["value"]), end="\r")
-
-            cb_id = self.add_value_callback(cbfoo)
-            self._currentChange = self.set_target_value(value)
-            self._currentChange.wait()
-        except KeyboardInterrupt:
-            self._currentChange.stop()
-            print(f"\nAborted change at (~) {self.get_current_value():1.5g}")
-        finally:
-            self.clear_value_callback(cb_id)
-        return self._currentChange
-
-    def update_change_relative(self, value, *args, **kwargs):
-        if (
-            hasattr(self, "_currentChange")
-            and self._currentChange
-            and not (self._currentChange.status() == "done")
-        ):
-            startvalue = self._currentChange.target
-        elif hasattr(self, "is_moving") and not self.is_moving():
-            startvalue = self.get_current_value(readback=True, *args, **kwargs)
-        else:
-            startvalue = self.get_current_value(*args, **kwargs)
-        self._currentChange = self.update_change(value + startvalue, *args, **kwargs)
-        return self._currentChange
-
-    Adj.update_change = update_change
-    Adj.update_change_relative = update_change_relative
-
-    return Adj
-
-
-# wrappers for adjustables <<<<<<<<<<<
-
-
-@spec_convenience
-class DummyAdjustable:
-    def __init__(self, name="no_adjustable"):
-        self.name = name
-        self.current_value = 0
-
-    def get_current_value(self):
-        return self.current_value
-
-    def set_target_value(self, value, hold=False):
-        def changer():
-            self.current_value = value
-        return Task(changer, hold=hold)
-
-    def __repr__(self):
-        name = self.name
-        cv = self.get_current_value()
-        s = f"{name} at value: {cv}" + "\n"
-        return s
-
-
-
-
-
-def _keywordChecker(kw_key_list_tups):
-    for tkw, tkey, tlist in kw_key_list_tups:
-        assert tkey in tlist, "Keyword %s should be one of %s" % (tkw, tlist)
-
 
 class PvRecord:
+
     def __init__(
         self, pvsetname, pvreadbackname=None, accuracy=None, name=None, elog=None
     ):
@@ -301,9 +161,11 @@ class PvRecord:
         return "%s is at: %s" % (self.Id, self.get_current_value())
 
 
-# @default_representation
+
+#@default_representation
 @spec_convenience
 class PvEnum:
+
     def __init__(self, pvname, name=None):
         self.Id = pvname
         self._pv = PV(pvname)
@@ -351,9 +213,11 @@ class PvEnum:
         return s
 
 
+
 @default_representation
 @spec_convenience
 class AdjustableVirtual:
+
     def __init__(
         self,
         adjustables,
@@ -370,7 +234,7 @@ class AdjustableVirtual:
                 try:
                     self.alias.append(adj.alias)
                 except Exception as e:
-                    logger.warning(f"could not find alias in {adj}")
+                    print(f"could not find alias in {adj}")
                     print(str(e))
         self._adjustables = adjustables
         self._foo_set_target_value_current_value = foo_set_target_value_current_value
@@ -415,3 +279,6 @@ class AdjustableVirtual:
             vals = self._foo_set_target_value_current_value(value)
             for adj, val in zip(self._adjustables, vals):
                 adj.set_current_value(val)
+
+
+
