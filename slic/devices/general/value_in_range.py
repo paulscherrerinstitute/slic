90216ab896318636626fe1e4734d15a604ac2bfa
Sven Augustin <sven.augustin@psi.ch>
2020-07-29 13:51:19 +0200

    rewrote ValueInRange, moved it to utils as RangeBar

--- slic/devices/general/value_in_range.py
+++ slic/devices/general/value_in_range.py
@@ -1,17 +1,10 @@
-import colorama
+from slic.utils.rangebar import RangeBar
 
 
 def update_changes(Adj):
-    def get_position_str(start, end, value):
-        s = ValueInRange(start, end, bar_width=30, unit="", fmt="1.5g").get_str(value)
-        return (
-            colorama.Style.BRIGHT
-            + f"{value:1.5}".rjust(10)
-            + colorama.Style.RESET_ALL
-            + "  "
-            + s
-            + 2 * "\t"
-        )
+
+    def get_position_str(start, stop, value):
+        return RangeBar(start, stop, width=30, units="", fmt="1.5g").get(value)
 
     def update_change(self, value):
         start = self.get_current_value()
@@ -46,57 +39,6 @@ def update_changes(Adj):
     Adj.update_change_relative = update_change_relative
 
     return Adj
-
-
-class ValueInRange:
-
-    def __init__(self, start_value, end_value, bar_width=30, unit="", fmt="1.5g"):
-        self.start_value = start_value
-        self.end_value = end_value
-        self.unit = unit
-        self.bar_width = bar_width
-        self._blocks = " ▏▎▍▌▋▊▉█"
-        self._fmt = fmt
-
-    def get_str(self, value):
-        if self.start_value == self.end_value:
-            frac = 1
-        else:
-            frac = (value - self.start_value) / (self.end_value - self.start_value)
-        return (
-            f"{self.start_value:{self._fmt}}"
-            + self.get_unit_str()
-            + "|"
-            + self.bar_str(frac)
-            + "|"
-            + f"{self.end_value:{self._fmt}}"
-            + self.get_unit_str()
-        )
-
-    def get_unit_str(self):
-        if not self.unit:
-            return ""
-        else:
-            return " " + self.unit
-
-    def bar_str(self, frac):
-        blocks = self._blocks
-        if 0 < frac and frac <= 1:
-            whole = int(self.bar_width // (1 / frac))
-            part = int((frac * self.bar_width - whole) // (1 / (len(blocks) - 1)))
-            return (
-                colorama.Fore.GREEN
-                + whole * blocks[-1]
-                + blocks[part]
-                + (self.bar_width - whole - 1) * blocks[0]
-                + colorama.Fore.RESET
-            )
-        elif frac == 0:
-            return self.bar_width * blocks[0]
-        elif frac < 0:
-            return colorama.Fore.RED + "<" * self.bar_width + colorama.Fore.RESET
-        elif frac > 1:
-            return colorama.Fore.RED + ">" * self.bar_width + colorama.Fore.RESET
 
 
 
---
+++ slic/utils/rangebar.py
@@ -0,0 +1,128 @@
+import colorama
+
+COLOR_GOOD  = colorama.Fore.GREEN
+COLOR_BAD   = colorama.Fore.RED
+COLOR_RESET = colorama.Fore.RESET
+
+STYLE_EMPH  = colorama.Style.BRIGHT
+STYLE_RESET = colorama.Style.RESET_ALL
+
+
+BLOCKS = " ▏▎▍▌▋▊▉█"
+NUM_BLOCKS = len(BLOCKS)
+BLOCK_EMPTY = BLOCKS[0]
+BLOCK_FULL = BLOCKS[-1]
+BLOCK_OVERFLOW = ">"
+BLOCK_UNDERFLOW = "<"
+BLOCK_ERROR = "X"
+
+
+class RangeBar:
+
+    def __init__(self, start, stop, width=30, units=None, fmt="1.5g"):
+        self.start = start
+        self.stop = stop
+        self.width = width
+        self.units = units
+        self.fmt = fmt
+
+
+    def get(self, value):
+        start = self.start
+        stop  = self.stop
+        width = self.width
+
+        part = calc_part(start, stop, value)
+        bar = make_bar_str(part, width)
+
+        value = self.format_number(value)
+        start = self.format_number(start)
+        stop  = self.format_number(stop)
+
+        value = style_emph(value)
+
+        return f"{value}   [ {start} |{bar}| {stop} ]"
+
+
+    def format_number(self, num):
+        fmt = self.fmt
+        units = self.units
+        units = f" {units}" if units else ""
+        return f"{num:{fmt}}{units}"
+
+
+    def __repr__(self):
+        start = self.format_number(self.start)
+        stop  = self.format_number(self.stop)
+        width = self.width
+        return f"[ {start} |{width} blocks| {stop} ]"
+
+
+
+def make_bar_str(part, width):
+    if 0 <= part <= 1:
+        return make_bar_str_good(part, width)
+    else:
+        return make_bar_str_bad(part, width)
+
+
+def make_bar_str_good(part, width):
+    whole, fract = split_whole_fract(width * part)
+    remain = width - whole - 1 # 1 for fract block
+
+    bar = BLOCK_FULL * whole
+
+    if remain >= 0: # if remain negative, add no empty fract block
+        ifract = calc_index(fract, NUM_BLOCKS)
+        bar += BLOCKS[ifract]
+        bar += BLOCK_EMPTY * remain
+
+    return color_good(bar)
+
+
+def make_bar_str_bad(part, width):
+    block = BLOCK_ERROR # just a safety measure
+
+    if part < 0:
+        block = BLOCK_UNDERFLOW
+    elif part > 1:
+        block = BLOCK_OVERFLOW
+
+    bar = block * width
+    return color_bad(bar)
+
+
+def calc_part(start, stop, value):
+    total_range = stop - start
+    if total_range == 0:
+        return 1
+    value_range = value - start
+    return value_range / total_range
+
+def split_whole_fract(value):
+    whole, fract = divmod(value, 1)
+    whole = int(whole)
+    return whole, fract
+
+def calc_index(fract, n_total):
+    index = fract * (n_total - 1)
+    return int(round(index))
+
+def color_good(string):
+    return COLOR_GOOD + string + COLOR_RESET
+
+def color_bad(string):
+    return COLOR_BAD + string + COLOR_RESET
+
+def style_emph(string):
+    return STYLE_EMPH + string + STYLE_RESET
+
+
+
+#TODO
+#- configurable blocks
+#- configurable colors
+#- monochrome?
+
+
+
