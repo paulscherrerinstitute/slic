5486420ff2b614b523f63e1beb3727911b82968a
Sven Augustin <sven.augustin@psi.ch>
2020-07-31 23:42:49 +0200

    added put method with progress bar to custom PV class; refactored repr of custom PV and added original repr as separate method

--- slic/utils/pv.py
+++ slic/utils/pv.py
@@ -1,13 +1,39 @@
 import epics
+from slic.utils import typename
+from slic.utils.rangebar import RangeBar
 
 
 class PV(epics.PV):
 
+    def put(self, value, *args, show_progress=False, **kwargs):
+        if not show_progress:
+            return super().put(value, *args, **kwargs)
+
+        kwargs["wait"] = True # enforce wait to show progress
+
+        start = self.get()
+        stop = value
+
+        with RangeBar(start, stop) as rbar:
+            def on_change(value=None, **kw):
+                rbar.show(value)
+
+            index = self.add_callback(on_change)
+            res = super().put(stop, *args, **kwargs)
+            self.remove_callback(index)
+
+        return res
+
+
     def __repr__(self):
+        tname = typename(self)
         name = self.pvname
         val = self.get()
-        units = self.units.decode()
-        return "PV \"{}\" at {} {}".format(name, val, units)
+        units = self.units
+        return f"{tname} \"{name}\" at {val} {units}"
+
+    def orig_repr(self):
+        return super().__repr__()
 
 
 
