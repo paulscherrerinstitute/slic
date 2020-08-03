26a450bca5ec89be5430c81bbd892fd114c69961
Sven Augustin <sven.augustin@psi.ch>
2020-08-03 12:29:12 +0200

    simplified code by using builtin issubclass

--- slic/utils/typecast.py
+++ slic/utils/typecast.py
@@ -2,26 +2,22 @@
 def downcast(obj, cls):
     """Downcast (specialize/narrow) object obj to child type cls"""
     obj_cls = type(obj)
-    _ensure_inheritance(obj_cls, cls)
+    _ensure_subclass(cls, obj_cls)
     return _cast(obj, cls)
 
 def upcast(obj, cls):
     """Upcast (generalize/widen) object obj to parent type cls"""
     obj_cls = type(obj)
-    _ensure_inheritance(cls, obj_cls)
+    _ensure_subclass(obj_cls, cls)
     return _cast(obj, cls)
 
-def _ensure_inheritance(parent_cls, child_cls):
-    if not _is_parent(parent_cls, child_cls):
-        _raise_error(parent_cls, child_cls)
+def _ensure_subclass(child_cls, parent_cls):
+    if not issubclass(child_cls, parent_cls):
+        _raise_error(child_cls, parent_cls)
 
-def _is_parent(parent_cls, child_cls):
-    all_parents = child_cls.__mro__
-    return parent_cls in all_parents
-
-def _raise_error(parent_cls, child_cls):
-    parent_cls_name = parent_cls.__name__
+def _raise_error(child_cls, parent_cls):
     child_cls_name = child_cls.__name__
+    parent_cls_name = parent_cls.__name__
     raise TypeError(f"class {child_cls_name} is not a subclass of {parent_cls_name}")
 
 def _cast(obj, cls):
