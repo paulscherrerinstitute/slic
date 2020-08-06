8210a4f2796f18fe9f19502cf3bddd188137b563
Sven Augustin <sven.augustin@psi.ch>
2020-08-06 18:24:02 +0200

    refactored Delay class using Adjustable as baseclass

--- slic/devices/general/delay_stage.py
+++ slic/devices/general/delay_stage.py
@@ -1,4 +1,5 @@
 from epics import PV
+from slic.core.adjustable import Adjustable
 from slic.devices.general.motor import Motor
 
 
@@ -11,9 +12,9 @@ CONVERSION_FACTOR_POS_TO_DELAY = 1 / CONVERSION_FACTOR_DELAY_TO_POS
 def delay_to_pos(delay):
     return delay * CONVERSION_FACTOR_DELAY_TO_POS
 
-
 def pos_to_delay(pos):
     return pos * CONVERSION_FACTOR_POS_TO_DELAY
+
 
 
 class DelayStage:
@@ -26,69 +27,65 @@ class DelayStage:
         return "{} | {}".format(self.motor, self.delay)
 
     def get(self):
-        res = {"motor": self.motor.get_current_value(), "delay": self.delay.get_current_value()}
+        res = {
+            "motor": self.motor.get_current_value(),
+            "delay": self.delay.get_current_value()
+        }
         return res
 
 
-class Delay:
 
-    def __init__(self, stage):
-        self._stage = stage
-        self.delay_stage_offset = 0.0
-        self.name = self._stage.name
-        self.Id = self._stage.pvname
+class Delay(Adjustable):
 
-    def delay_to_motor(self, delay):
-        motor_pos = delay_to_pos(delay)
-        return motor_pos
+    def __init__(self, motor):
+        self._motor = motor
+        self.name = motor.name
+        self.pvname = motor.pvname
+        self.offset_pos = 0
 
-    def get_current_value(self):
-        """ Adjustable convention"""
-        motor_pos = self._stage.get_current_value()
-        motor_pos -= self.delay_stage_offset
-        delay = pos_to_delay(motor_pos)
+    @property
+    def current_task(self):
+        return self._motor.current_task
+
+
+    def get_current_value(self, *args, **kwargs):
+        pos = self._motor.get_current_value(*args, **kwargs)
+        return self._pos_to_delay(pos)
+
+    def reset_current_value_to(self, value, *args, **kwargs):
+        pos = self._delay_to_pos(value)
+        return self._motor.reset_current_value_to(pos, *args, **kwargs)
+
+    def set_target_value(self, value, *args, **kwargs):
+        pos = self._delay_to_pos(value)
+        return self._motor.set_target_value(pos, *args, **kwargs)
+
+
+    def _delay_to_pos(self, delay):
+        pos = delay_to_pos(delay)
+        pos += self.offset_pos
+        return pos
+
+    def _pos_to_delay(self, pos):
+        pos -= self.offset_pos
+        delay = pos_to_delay(pos)
         return delay
 
-    def set_current_value(self, value):
-        motor_pos = self.delay_to_motor(value) + self.delay_stage_offset
-        self._stage.set_current_value(motor_pos)
-        return (value, motor_pos)
 
-    def set_target_value(self, value, hold=False, check=True):
-        value = self.delay_to_motor(value) + self.delay_stage_offset
-        delay = pos_to_delay(value - self.delay_stage_offset)
-        return self._stage.set_target_value(value, hold, check)
-
-    def gui(self, guiType="xdm"):
-        return self._stage.gui()
-
-    # spec-inspired convenience methods
-    def mv(self, value):
-        self._stage._currentChange = self.set_target_value(value)
-
-    def wm(self, *args, **kwargs):
-        return self.get_current_value(*args, **kwargs)
-
-    def mvr(self, value, *args, **kwargs):
-        motor_pos = self.delay_to_motor(value)
-        self._stage.mvr(motor_pos)
-
-    def wait(self):
-        self._stage._currentChange.wait()
+    def is_moving(self):
+        return self._motor.is_moving()
 
     def stop(self):
-        """ Adjustable convention"""
-        try:
-            self._stage._currentChange.stop()
-        except:
-            self._stage.stop()
+        return self._motor.stop()
 
-    # return string with motor value as variable representation
+    def gui(self):
+        return self._motor.gui()
+
+
     def __repr__(self):
-        return "Delay at %s sec" % self.wm()
-
-    def __call__(self, value):
-        self._currentChange = self.set_target_value(value)
+        res = super().__repr__()
+        res += " sec"
+        return res
 
 
 
