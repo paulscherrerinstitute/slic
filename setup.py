0fcad9fd716ca5302b5a25b65278fed4012b93ba
Sven Augustin <sven.augustin@psi.ch>
2020-09-11 18:13:29 +0200

    Release v0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="slic",
-    version="0.0.2",
+    version="0.0.3",
     url="https://gitlab.psi.ch/slic/slic",
     description="SwissFEL Library for Instrument Control",
     author="Paul Scherrer Institute",
