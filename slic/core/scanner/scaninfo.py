81face1a6a6206e3268b6b5e57e9260c482f113f
Sven Augustin <sven.augustin@psi.ch>
2020-09-12 15:05:46 +0200

    added preliminary output of dict as expected by SF_DAQ to ScanInfo

--- slic/core/scanner/scaninfo.py
+++ slic/core/scanner/scaninfo.py
@@ -6,12 +6,13 @@ from slic.utils import json_save
 class ScanInfo:
 
     def __init__(self, filename_base, base_dir, adjustables, values, suffix="_scan_info.json"):
+        self.filename_base = filename_base
         self.base_dir = base_dir
         self.filename = os.path.join(base_dir, filename_base)
         self.filename += suffix
 
-        names = [ta.name if hasattr(ta, "name") else "noName" for ta in adjustables] #TODO else None?
-        ids =   [ta.Id   if hasattr(ta, "Id")   else "noId"   for ta in adjustables]
+        self.names = names = [ta.name if hasattr(ta, "name") else "noName" for ta in adjustables] #TODO else None?
+        self.ids   = ids =   [ta.Id   if hasattr(ta, "Id")   else "noId"   for ta in adjustables]
         self.parameters = {"name": names, "Id": ids}
 
         self.values_all = values
@@ -47,6 +48,36 @@ class ScanInfo:
             "scan_info":       self.info
         }
         return scan_info_dict
+
+
+    def to_sfdaq_dict(self):
+        scan_name = self.filename_base.replace("/", "_") #TODO: better use "__" to easier see difference to actual _ in original string?
+
+        #TODO: not every adjustable needs/has a raw/user/dial distinction (also readbacks_raw below)
+        num_adjustables = len(self.ids)
+        offsets      = [0] * num_adjustables
+        coefficients = [1] * num_adjustables
+
+        #TODO: store last_* separately during append()?
+        values    = self.values
+        readbacks = self.readbacks
+        last_values    = values[-1]    if values    else None
+        last_readbacks = readbacks[-1] if readbacks else None
+
+        scan_info_dict = {
+            "scan_name":             scan_name,
+
+            "motors_name":           self.names,
+            "motors_pv_name":        self.ids,
+            "motors_offset":         offsets,
+            "motors_coefficient":    coefficients,
+
+            "motors_value":          last_values,
+            "motors_readback_value": last_readbacks,
+            "motors_readback_raw":   last_readbacks #TODO
+        }
+        return scan_info_dict
+
 
     def __repr__(self):
         return "Scan info in {}".format(self.filename)
