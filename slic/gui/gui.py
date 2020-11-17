0866185ed9c77b9c9cf9bedef04037bd37c26615
Sven Augustin <sven.augustin@psi.ch>
2020-11-17 12:54:02 +0100

    added a GUI class (just a deferred call to run())

--- slic/gui/gui.py
+++ slic/gui/gui.py
@@ -3,6 +3,17 @@ import wx
 from .daqframe import DAQFrame
 
 
+class GUI:
+
+    def __init__(self, *args, **kwargs):
+        self.args = args
+        self.kwargs = kwargs
+
+    def __call__(self):
+        run(*self.args, **self.kwargs)
+
+
+
 def run(*args, **kwargs):
     app = wx.App()
     frame = DAQFrame(*args, **kwargs)
