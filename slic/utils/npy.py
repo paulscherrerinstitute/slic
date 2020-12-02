b0d5969dbbf884366374a4fef6693c0c3a6f35a1
Sven Augustin <sven.augustin@psi.ch>
2020-12-02 14:37:34 +0100

    added some variations of nice_steps

--- slic/utils/npy.py
+++ slic/utils/npy.py
@@ -17,16 +17,41 @@ def nice_linspace(start, stop, num, **kwargs):
     return np.linspace(start, stop, num + 1, **kwargs)
 
 
-def nice_steps(start, stop, step=1, endpoint=True, **kwargs):
+def nice_steps_centered(start, stop, step=1, endpoint=True, **kwargs):
     start, stop, step = normalize_direction(start, stop, step)
 
-    start = istep(start, step)
-    stop  = istep(stop, step)
+    istart = istep(start, step)
+    istop  = istep(stop, step)
 
     if endpoint:
-        stop += 1
+        istop += 1
 
-    return step * np.arange(start, stop, **kwargs)
+    return step * np.arange(istart, istop, **kwargs)
+
+
+def nice_steps_left_aligned(start, stop, step=1, endpoint=True, **kwargs):
+    start, stop, step = normalize_direction(start, stop, step)
+
+    delta = stop - start
+    idelta = istep(delta, step)
+
+    if endpoint:
+        idelta += 1
+
+    return start + step * np.arange(idelta, **kwargs)
+
+
+def nice_steps_right_aligned(start, stop, step=1, endpoint=True, **kwargs):
+    start, stop, step = normalize_direction(start, stop, step)
+
+    delta = stop - start
+    idelta = istep(delta, step)
+
+    if endpoint:
+        idelta += 1
+
+    steps = stop - step * np.arange(idelta, **kwargs)
+    return steps[::-1]
 
 
 def normalize_direction(start, stop, step):
