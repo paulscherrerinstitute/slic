c58f7459f32bc1c9b0a28d9c1e71d238c51acb96
Sven Augustin <sven.augustin@psi.ch>
2021-05-10 12:20:32 +0200

    added optional jitter to dummy adj

--- slic/core/adjustable/dummyadjustable.py
+++ slic/core/adjustable/dummyadjustable.py
@@ -1,14 +1,19 @@
+from random import random
 from .adjustable import Adjustable
 
 
 class DummyAdjustable(Adjustable):
 
-    def __init__(self, initial_value=0, name="Dummy", units=None):
+    def __init__(self, initial_value=0, name="Dummy", units=None, jitter=False):
         super().__init__(name=name, units=units)
         self._current_value = initial_value
+        self._jitter = jitter
 
     def get_current_value(self):
-        return self._current_value
+        value = self._current_value
+        if self._jitter:
+            value += round(random(), 1)
+        return value
 
     def set_target_value(self, value, hold=False):
         def change():
