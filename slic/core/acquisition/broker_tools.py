c53cf5c686a3764158071b5246c5b3c8eba87966
NichtJens <nichtjens@gmail.com>
2021-07-29 13:07:30 +0200

    made it easier to test SFAcquisition by allowing fake PIDs again (a warning will be logged for each call)

--- slic/core/acquisition/broker_tools.py
+++ slic/core/acquisition/broker_tools.py
@@ -1,78 +1,95 @@
 from datetime import datetime
 import socket
 import epics
+from logzero import logger as log
 
 from slic.utils import singleton
 
 
 #TODO: these should probably move to different IOCs
-PULSEID_PVS = {
+ENDSTATION_TO_PULSEID_PVS = {
     "alvra"  : "SLAAR11-LTIM01-EVR0:RX-PULSEID",
     "bernina": "SLAAR21-LTIM01-EVR0:RX-PULSEID",
     "maloja" : "SLAAR11-LTIM01-EVR0:RX-PULSEID",
     "furka":   "SLAAR11-LTIM01-EVR0:RX-PULSEID"
 }
 
-IP_TO_BEAMLINE = {
+IP_TO_ENDSTATION = {
     "129.129.242": "alvra",
     "129.129.243": "bernina",
     "129.129.246": "maloja",
     "129.129.247": "furka"
 }
 
-
-def get_beamline():
-    ip = socket.gethostbyname(socket.gethostname())
-    key = ip[:11] #TODO: split at "." ?
-    return IP_TO_BEAMLINE.get(key)
+#TODO: these need an update
+REFERENCE_DATETIME = datetime(2020, 5, 8, 8, 29, 52)
+REFERENCE_PID = 11718051371
 
 
-#def get_fake_current_pulseid():
-#    # 2020-05-08 08:29:52.742737 : 11718049010
-#    # 2020-05-28 - checked compared to "real" pulse-id: 2380 pulses difference
-#    reference_date = datetime(2020, 5, 8, 8, 29, 52)
-#    now = datetime.utcnow()
-#    delta_t = now - reference_date
-#    delta_p = delta_t.total_seconds() * 100
-#    delta_p = int(delta_p)
-#    pid = delta_p + 11718049010 + 2361
-#    return pid
+
+def decide_get_current_pulseid():
+    try:
+        endstation = get_endstation()
+        pv = get_pulseid_pv(endstation)
+    except (ValueError, RuntimeError) as e:
+        log.error(e)
+        return get_fake_current_pulseid
+    else:
+        return pv.get
+
+
+def get_pulseid_pv(endstation):
+    try:
+        pv_name = ENDSTATION_TO_PULSEID_PVS[endstation]
+    except KeyError as e:
+        raise ValueError(f"cannot assign Pulse ID PV to endstation \"{endstation}\"") from e
+
+    pv = epics.PV(pv_name)
+    if not pv.wait_for_connection(timeout=0.1):
+        raise RuntimeError(f"could not connect to Pulse ID PV \"{pv_name}\" for endstation \"{endstation}\"")
+
+    return pv
+
+
+def get_endstation():
+    name = socket.gethostname()
+    ip = socket.gethostbyname(name)
+    key = cut_after_nth(ip, ".", 3)
+    try:
+        return IP_TO_ENDSTATION[key]
+    except KeyError as e:
+        raise RuntimeError(f"cannot assign endstation to IP {ip} ({name})") from e
+
+
+def cut_after_nth(string, sep, n):
+    split = string.split(sep, n)
+    split = split[:n]
+    return sep.join(split)
+
+
+def get_fake_current_pulseid():
+    log.warning("using fake Pulse ID")
+    now = datetime.utcnow()
+    delta_t = now - REFERENCE_DATETIME
+    delta_p = delta_t.total_seconds() * 100
+    delta_p = int(round(delta_p))
+    pid = REFERENCE_PID + delta_p
+    return pid
 
 
 
 @singleton
 class get_current_pulseid():
-#    """
-#    This is a "pretend function" that tries to use the correct PV to retrieve the PID
-#    If the PV cannot connect, a fake PID is returned that is calculated from the time
-#    The implementation only does/needs one connection check at the start
-#    """
-
-#    def __init__(self):
-#        self.get = get_fake_current_pulseid
-#        beamline = get_beamline()
-#        pulseid_pv = PULSEID_PVS.get(beamline)
-#        if pulseid_pv:
-#            pv = epics.PV(pulseid_pv)
-#            if pv.wait_for_connection():
-#                self.get = pv.get
-
     """
-    This is a "pretend function" that tries to use the correct PV to retrieve the PID
-    If the PV name is not known, a ValueError is raised
-    If the PV does not connect, a RunTime is raised
-    The implementation only does/needs one connection check at the start
+    This is a "pretend function" that tries to use the correct PV to retrieve the PID.
+    If the IP cannot be mapped to an endstation, the endstation cannot be mapped to a PV, or the PV does not connect,
+    the respective error is logged and a fake (and probably wrong) PID is returned that is calculated from the time.
+    The fake PID logs a warning on every call.
+    The implementation only does/needs one connection check at the start.
     """
 
     def __init__(self):
-        beamline = get_beamline()
-        pulseid_pv = PULSEID_PVS.get(beamline)
-        if not pulseid_pv:
-            raise ValueError(f"no Pulse ID PV known for beamline \"{beamline}\"")
-        pv = epics.PV(pulseid_pv)
-        if not pv.wait_for_connection():
-            raise RuntimeError(f"could not connect to Pulse ID PV \"{pulseid_pv}\"")
-        self.get = pv.get
+        self.get = decide_get_current_pulseid()
 
     def __call__(self):
         return int(self.get())
