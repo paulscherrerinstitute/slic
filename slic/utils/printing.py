b7bff450d72659607398e3f0f74067662c0ef7bc
Sven Augustin <sven.augustin@psi.ch>
2021-08-23 14:54:11 +0200

    fix for non-string keys

--- slic/utils/printing.py
+++ slic/utils/printing.py
@@ -5,8 +5,8 @@ def printable_dict_of_dicts(d):
 
 
 def printable_dict(d, header=None):
-    length = maxlen(d) + 1
-    lines = sorted("{}:{}{}".format(k, " "*(length-len(k)), v) for k, v in d.items())
+    length = maxstrlen(d) + 1
+    lines = sorted("{}:{}{}".format(k, " "*(length-strlen(k)), v) for k, v in d.items())
 
     if header:
         header = format_header(header)
@@ -15,10 +15,17 @@ def printable_dict(d, header=None):
     return "\n".join(lines) + "\n"
 
 
+def maxstrlen(seq):
+    seq = [str(i) for i in seq]
+    return maxlen(seq)
+
 def maxlen(seq):
     if not seq: # max of empty sequence is a ValueError
         return 0
     return max(len(i) for i in seq)
+
+def strlen(x):
+    return len(str(x))
 
 
 def format_header(msg, line="-"):
