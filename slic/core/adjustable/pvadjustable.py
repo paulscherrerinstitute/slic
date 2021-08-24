ce4a031de4178a46e75942662ac9058c8f84298a
Sven Augustin <sven.augustin@psi.ch>
2021-08-24 18:11:59 +0200

    adapted to PVChangeMonitor

--- slic/core/adjustable/pvadjustable.py
+++ slic/core/adjustable/pvadjustable.py
@@ -1,13 +1,20 @@
 import time
 from types import SimpleNamespace
 from epics import PV
-from .adjustable import Adjustable, AdjustableError
+
 from slic.utils import typename
+
+from .adjustable import Adjustable, AdjustableError
+from .pvchangemon import PVChangeMonitor
 
 
 class PVAdjustable(Adjustable):
 
-    def __init__(self, pvname_setvalue, pvname_readback=None, pvname_stop=None, pvname_done_moving=None, pvname_moving=None, accuracy=None, active_move=False, name=None, internal=False):
+    def __init__(self,
+        pvname_setvalue, pvname_readback=None, pvname_stop=None, pvname_done_moving=None, pvname_moving=None, 
+        accuracy=None, active_move=False, process_time=0, wait_time=0.1, timeout=60, 
+        name=None, internal=False
+    ):
         pv_setvalue = PV(pvname_setvalue)
         pv_readback = PV(pvname_readback) if pvname_readback else pv_setvalue
 
@@ -17,7 +24,9 @@ class PVAdjustable(Adjustable):
 
         self.accuracy = accuracy
         self.active_move = active_move
-        self._change_requested = False
+        self.process_time = process_time
+        self.wait_time = wait_time
+        self.timeout = timeout
 
         self.pvnames = SimpleNamespace(
             setvalue = pvname_setvalue,
@@ -29,22 +38,14 @@ class PVAdjustable(Adjustable):
             readback = pv_readback
         )
 
-         #TODO: skip optional PVs or set them to None?
+        #TODO: skip optional PVs or set them to None?
 
         if pvname_stop:
             pv_stop = PV(pvname_stop)
             self.pvnames.stop = pvname_stop
             self.pvs.stop = pv_stop
 
-        if pvname_done_moving:
-            pv_done_moving = PV(pvname_done_moving)
-            self.pvnames.done_moving = pvname_done_moving
-            self.pvs.done_moving = pv_done_moving
-
-        if pvname_moving:
-            pv_moving = PV(pvname_moving)
-            self.pvnames.moving = pvname_moving
-            self.pvs.moving = pv_moving
+        self._pcm = make_pcm(pvname_done_moving, pvname_moving)
 
 
     def get_current_value(self, readback=True):
@@ -59,37 +60,36 @@ class PVAdjustable(Adjustable):
         return self._as_task(change, stopper=self._stop, hold=hold)
 
 
-    def _change(self, value, wait_time=0.1, timeout=60):
-        timeout += time.time()
+    def _change(self, value):
+        timeout = self.timeout + time.time()
+        wait_time = self.wait_time
 
-        self._change_requested = True
+        # wait for ready
+        for _ in self._pcm.start():
+#            print(self._pcm.state)
+#            print("waiting for: ready")
+            time.sleep(wait_time)
+            if time.time() >= timeout:
+                self._pcm.stop()
+                tname = typename(self)
+                raise AdjustableError(f"waiting for {tname} \"{self.name}\" to be ready for change to {value} {self.units} timed out")
+
         self.pvs.setvalue.put(value, wait=True, use_complete=True) # use_complete=True enables status in PV.put_complete
+        time.sleep(self.process_time)
 
-        # wait for start
-        while self._change_requested and not self._is_close() and not self.is_moving():
+        # wait for done
+        for _ in self._pcm.wait():
+#            print(self._pcm.state)
+#            print("waiting for: done")
             time.sleep(wait_time)
-            if self.active_move:
-                self.pvs.setvalue.put(value, wait=True, use_complete=True)
-            if time.time() >= timeout:
+            if self._pcm.state == "ready" and self._is_close():
                 self._stop()
-                tname = typename(self)
-                raise AdjustableError(f"starting to change {tname} \"{self.name}\" to {value} {self.units} timed out")
-
-        # wait for move done
-        while self._change_requested and self.is_moving():
-            time.sleep(wait_time)
-            if self.active_move:
-                self.pvs.setvalue.put(value, wait=True, use_complete=True)
-            if time.time() >= timeout:
-                self._stop()
-                tname = typename(self)
-                raise AdjustableError(f"waiting for move done {tname} \"{self.name}\" to {value} {self.units} timed out")
-
-        self._change_requested = False
+                print("seems we are already there")
+                break
 
 
     def _stop(self):
-        self._change_requested = False
+        self._pcm.stop()
         pv_stop = self._get_pv("stop")
         if pv_stop:
             pv_stop.put(1, wait=True)
@@ -99,28 +99,20 @@ class PVAdjustable(Adjustable):
         if not self.pvs.setvalue.put_complete:
             return True
 
-        pv_done_moving = self._get_pv("done_moving")
-        if pv_done_moving:
-            done = pv_done_moving.get()
-            if not bool(done):
-                return True
-
-        pv_moving = self._get_pv("moving")
-        if pv_moving:
-            moving = pv_moving.get()
-            if bool(moving):
-                return True
+        if self._pcm:
+            return self._pcm.is_changing
 
         return not self._is_close()
 
 
     def _is_close(self):
+        setvalue = self.pvs.setvalue.get()
+        readback = self.pvs.readback.get()
         if self.accuracy is not None:
-            setvalue = self.pvs.setvalue.get()
-            readback = self.pvs.readback.get()
             delta = abs(setvalue - readback)
             return (delta <= self.accuracy)
-        return True
+        else:
+            return setvalue == readback
 
 
     def stop(self):
@@ -132,6 +124,20 @@ class PVAdjustable(Adjustable):
 
     def _get_pv(self, name):
         return vars(self.pvs).get(name)
+
+
+
+
+
+def make_pcm(pvname_done_moving, pvname_moving):
+    if None not in (pvname_done_moving, pvname_moving):
+        raise ValueError("please provide only pvname_done_moving or pvname_moving, but not both")
+
+    if pvname_moving:
+        return PVChangeMonitor(pvname_moving, inverted=False)
+
+    if pvname_done_moving:
+        return PVChangeMonitor(pvname_done_moving, inverted=True)
 
 
 
