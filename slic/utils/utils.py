27032b1778c3a548918c7c87cfdaaf4844406e99
Sven Augustin <sven.augustin@psi.ch>
2021-10-06 19:21:23 +0200

    added natural sorting

--- slic/utils/utils.py
+++ slic/utils/utils.py
@@ -21,3 +21,12 @@ def iround(val):
 
 
 
+import re
+
+def sorted_naturally(iterable, reverse=False):
+    re_digits = re.compile("([0-9]+)")
+    natural = lambda item: [int(c) if c.isdigit() else c.casefold() for c in re_digits.split(item)] 
+    return sorted(iterable, key=natural, reverse=reverse)
+
+
+
