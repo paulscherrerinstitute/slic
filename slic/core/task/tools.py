808bd4785c759b7cdad3333befb35f0de4737ee0
Sven Augustin <sven.augustin@psi.ch>
2021-10-26 19:24:44 +0200

    fix signature

--- slic/core/task/tools.py
+++ slic/core/task/tools.py
@@ -1,7 +1,9 @@
 from functools import wraps
+from slic.utils import forwards_to
 
 
 def task_producer(obj, func):
+    @forwards_to(func, nfilled=1) # nfilled=1 to remove self
     @wraps(func)
     def wrapper(*args, hold=False, **kwargs):
         filled_func = lambda: func(*args, **kwargs)
