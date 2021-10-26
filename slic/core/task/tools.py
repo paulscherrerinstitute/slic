adbdfa6ffc1da610a90fe8e96c3d87ee108fa0a6
Sven Augustin <sven.augustin@psi.ch>
2021-10-26 19:42:03 +0200

    added starter/stopper to task_producer arguments, forward to _as_task

--- slic/core/task/tools.py
+++ slic/core/task/tools.py
@@ -2,12 +2,12 @@ from functools import wraps
 from slic.utils import forwards_to
 
 
-def task_producer(obj, func):
+def task_producer(obj, func, starter=None, stopper=None):
     @forwards_to(func, nfilled=1) # nfilled=1 to remove self
     @wraps(func)
     def wrapper(*args, hold=False, **kwargs):
         filled_func = lambda: func(*args, **kwargs)
-        return obj._as_task(filled_func, hold=hold)
+        return obj._as_task(filled_func, starter=starter, stopper=stopper, hold=hold)
     return wrapper
 
 
