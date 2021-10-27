b41c0b55a242c7652bd88c65dc7740380ef95363
Sven Augustin <sven.augustin@psi.ch>
2021-10-27 12:29:45 +0200

    added a process time

--- slic/core/adjustable/dummyadjustable.py
+++ slic/core/adjustable/dummyadjustable.py
@@ -1,13 +1,15 @@
 from random import random
+from time import sleep
 from .adjustable import Adjustable
 
 
 class DummyAdjustable(Adjustable):
 
-    def __init__(self, initial_value=0, jitter=False, ID="DUMMY", name="Dummy", units=None):
+    def __init__(self, initial_value=0, jitter=False, process_time=0, ID="DUMMY", name="Dummy", units=None):
         super().__init__(ID, name=name, units=units)
         self._current_value = initial_value
         self._jitter = jitter
+        self._process_time = process_time
 
     def get_current_value(self):
         value = self._current_value
@@ -17,6 +19,7 @@ class DummyAdjustable(Adjustable):
         return value
 
     def set_target_value(self, value):
+        sleep(self._process_time)
         self._current_value = value
         print(repr(self))
 
