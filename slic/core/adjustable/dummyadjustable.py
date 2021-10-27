d4fcf2434498335c3cd6ece78f7ab1d1f9d79c21
Sven Augustin <sven.augustin@psi.ch>
2021-10-27 12:23:27 +0200

    made jitter configurable

--- slic/core/adjustable/dummyadjustable.py
+++ slic/core/adjustable/dummyadjustable.py
@@ -4,15 +4,16 @@ from .adjustable import Adjustable
 
 class DummyAdjustable(Adjustable):
 
-    def __init__(self, initial_value=0, ID="DUMMY", name="Dummy", units=None, jitter=False):
+    def __init__(self, initial_value=0, jitter=False, ID="DUMMY", name="Dummy", units=None):
         super().__init__(ID, name=name, units=units)
         self._current_value = initial_value
         self._jitter = jitter
 
     def get_current_value(self):
         value = self._current_value
-        if self._jitter:
-            value += round(random(), 1)
+        jitter = self._jitter
+        if jitter:
+            value += plus_minus() * float(jitter)
         return value
 
     def set_target_value(self, value):
@@ -21,6 +22,11 @@ class DummyAdjustable(Adjustable):
 
     def is_moving(self):
         return False
+
+
+
+def plus_minus():
+    return 2 * random() - 1
 
 
 
