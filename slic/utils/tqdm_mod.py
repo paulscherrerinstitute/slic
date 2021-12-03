a2674180d3774c833853f675c1ee1c4014d74849
Sven Augustin <sven.augustin@psi.ch>
2021-12-03 14:38:35 +0100

    set default units to Hz; format rate such that it does not jump around at <= 100 Hz

--- slic/utils/tqdm_mod.py
+++ slic/utils/tqdm_mod.py
@@ -2,7 +2,25 @@ from time import sleep
 import tqdm
 
 
+def tqdm_sleep(seconds, ndiv=100):
+    delta = seconds / float(ndiv)
+    for _ in tqdm.trange(ndiv):
+        sleep(delta)
+
+
+
 class tqdm_mod(tqdm.tqdm):
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("unit", "@")        # use "@/s" to signal Hz
+        kwargs.setdefault("unit_scale", True) # this enables use of format_sizeof
+        super().__init__(*args, **kwargs)
+
+    def format_meter(self, *args, **kwargs):
+        res = super().format_meter(*args, **kwargs)
+        # these have to have the same length otherwise the combined line gets messed up
+        res = res.replace("@/s", " Hz")
+        return res
 
     def set(self, elapsed):
         """
@@ -19,11 +37,18 @@ def clamp(val, vmin, vmax):
     return val
 
 
+def format_sizeof(num, *args, **kwargs):
+    # format floats such that they accommodate up to 100.x without jumping around
+    if isinstance(num, float):
+        return f"{num:5.1f}" # len("100") + len(".1") == 3+2 == 5
+    # for everything else, use the default string representation
+    return str(num)
 
-def tqdm_sleep(seconds, ndiv=100):
-    delta = seconds / float(ndiv)
-    for _ in tqdm.trange(ndiv):
-        sleep(delta)
+
+
+# format_meter is a staticmethod, thus has no self and uses the tqdm class instead
+# hence need to overwrite at the source to convince it to use the custom format_sizeof
+tqdm.tqdm.format_sizeof = format_sizeof
 
 
 
