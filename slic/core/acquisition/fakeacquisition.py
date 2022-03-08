abe2c4af31153ca342fb595b257d9c6f8a413586
Sven Augustin <sven.augustin@psi.ch>
2022-03-08 15:54:05 +0100

    added a FakeClient to manage a (fake) run number

--- slic/core/acquisition/fakeacquisition.py
+++ slic/core/acquisition/fakeacquisition.py
@@ -15,7 +15,10 @@ class FakeAcquisition(BaseAcquisition):
         self.instrument = instrument
         self.pgroup = pgroup
 
+        self.client = FakeClient()
+
         self.default_data_base_dir = "static_data"
+        self.default_dir = ""
 
         self.default_detectors = shuffle([f"JFJFJFJFJFJFJFJFJFJF{i}" for i in range(2)])
         self.default_channels  = shuffle([f"CHCHCHCHCH{i}" for i in range(100)])
@@ -24,7 +27,14 @@ class FakeAcquisition(BaseAcquisition):
         self._stop()
 
 
-    def acquire(self, filename, data_base_dir=None, detectors=None, channels=None, pvs=None, scan_info=None, n_pulses=100, continuous=False, wait=True):
+    def acquire(self, filename, data_base_dir=None, detectors=None, channels=None, pvs=None, scan_info=None, n_pulses=100, continuous=False, is_scan_step=False, wait=True):
+        if not is_scan_step:
+            run_number = self.client.next_run()
+            print(f"Advanced run number to {run_number}.")
+        else:
+            run_number = self.client.run_number
+            print(f"Continuing run number {run_number}.")
+
         if data_base_dir is None:
             print("No base directory specified, using default base directory.")
             data_base_dir = self.default_data_base_dir
@@ -69,6 +79,17 @@ class FakeAcquisition(BaseAcquisition):
 def shuffle(seq):
     random.shuffle(seq)
     return seq
+
+
+
+class FakeClient:
+
+    run_number = 0
+
+    def next_run(self):
+        self.run_number += 1
+        print("run number is", self.run_number)
+        return self.run_number
 
 
 
