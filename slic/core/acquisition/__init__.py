7685f8e1390acb620cee3f2fa75cbd60b1506fd4
Sven Augustin <sven.augustin@psi.ch>
2022-03-09 21:15:59 +0100

    soft-deprecated DIA; lowered python version to 3.8 for cam_server_client

--- slic/core/acquisition/__init__.py
+++ slic/core/acquisition/__init__.py
@@ -1,7 +1,7 @@
 
 from .bsacquisition import BSAcquisition
 from .dbacquisition import DBAcquisition
-from .diaacquisition import DIAAcquisition
+#from .diaacquisition import DIAAcquisition #TODO: deprecated?
 from .sfacquisition import SFAcquisition
 from .pvacquisition import PVAcquisition
 
