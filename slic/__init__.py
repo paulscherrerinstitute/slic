bb78b7ba2473adf2e5c06bd0d56733a31627f50b
Sven Augustin <sven.augustin@psi.ch>
2022-07-05 14:44:33 +0200

    added terminal title and rich pretty printing

--- slic/__init__.py
+++ slic/__init__.py
@@ -3,10 +3,20 @@ from . import core
 from . import devices
 from . import utils
 
+from .utils.termtitle import termtitle as _termtitle
+_termtitle("⊚slic")
+
 from .utils.logcfg import logcfg as _logcfg
 _logcfg()
 
 from .utils.pvpreload import pvpreload as _pvpreload
 _pvpreload()
+
+#TODO: add rich to dependency list
+try:
+    from .utils.richcfg import richcfg as _richcfg
+    _richcfg()
+except Exception as e:
+    print("setting up rich failed:", e)
 
 
