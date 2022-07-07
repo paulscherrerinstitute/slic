348389eda420ee6c2dcdcf5c92b2d94e53183d88
Sven Augustin <sven.augustin@psi.ch>
2022-07-07 16:52:53 +0200

    cleaned up huber

--- slic/devices/endstations/alvra_huber.py
+++ slic/devices/endstations/alvra_huber.py
@@ -1,21 +1,14 @@
+from slic.core.device import Device
 from slic.devices.general.motor import Motor
 
 
-class Huber:
+class Huber(Device):
 
-    def __init__(self, ID, alias_namespace=None, z_undulator=None, description=None, name="Prime Sample Manipulator"):
-        self.ID = ID
-
-        ### Huber sample stages ###
-        self.x = Motor(ID + ":MOTOR_X1", name + " X")
-        self.y = Motor(ID + ":MOTOR_Y1", name + " Y")
-        self.z = Motor(ID + ":MOTOR_Z1", name + " Z")
-
-    def __str__(self):
-        return "Huber Sample Stage %s\nx: %s mm\ny: %s mm\nz: %s mm" % (self.ID, self.x.wm(), self.y.wm(), self.z.wm())
-
-    def __repr__(self):
-        return "{'X': %s, 'Y': %s, 'Z': %s}" % (self.x.wm(), self.y.wm(), self.z.wm())
+    def __init__(self, ID, name="Huber Sample Stages", **kwargs):
+        super().__init__(ID, name=name, **kwargs)
+        self.x = Motor(ID + ":MOTOR_X1", name = name + " X")
+        self.y = Motor(ID + ":MOTOR_Y1", name = name + " Y")
+        self.z = Motor(ID + ":MOTOR_Z1", name = name + " Z")
 
 
 
