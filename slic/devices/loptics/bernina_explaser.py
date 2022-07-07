69b7ee9d75817ecc68e2d7dd555d13d27a48232f
Sven Augustin <sven.augustin@psi.ch>
2022-07-08 00:04:24 +0200

    removed redundant "old" version

--- slic/devices/loptics/bernina_explaser.py
+++ slic/devices/loptics/bernina_explaser.py
@@ -110,104 +110,19 @@ class ExpLaser:
         # Implementation of delay compensation, this assumes for now that delays_glob and delay_tt actually delay in positive directions.
         self.delay_lxtt = DelayCompensation([self.delay_glob, self.delay_tt], [-1, 1], name="delay_lxtt")
 
-        # compressor
         self.compressor = Motor(ID + "-M532:MOT")
-#        self.compressor = Motor(ID + '-M532:MOT')
 
-        # LAM delay stages
         self.lam_delay_smar_stg = SmarActAxis("SLAAR21-LMTS-LAM11")
         self.lam_delay_smar = DelayStage(self.lam_delay_smar_stg)
-#        self.lam_delay_stg_smar = SmarActAxis('SLAAR21-LMTS-LAM11')
-#        self.lam_delay_smar = DelayStage(self.lam_delay_stg_smar)
 
         self.lam_delay_stg = Motor(ID + "-M548:MOT")
         self.lam_delay = DelayStage(self.lam_delay_stg)
-#        self.lam_delay_stg = Motor(ID + '-M548:MOT')
-#        self.lam_delay = DelayStage(self.lam_delay_stg)
 
-        # PALM delay stages
         self.palm_delay_stg = Motor(ID + "-M552:MOT")
         self.palm_delay = DelayStage(self.palm_delay_stg)
-#        self.palm_delay_stg = Motor(ID + '-M552:MOT')
-#        self.palm_delay = DelayStage(self.palm_delay_stg)
 
-        # PSEN delay stages
-#        self.psen_delay_stg = Motor(ID + '')
-#        self.psen_delay = DelayStage(self.pump_delay_stg)
         self.psen_delay_stg = Motor(ID + "-M561:MOT")
         self.psen_delay = DelayStage(self.psen_delay_stg)
-
-        # SmarActID
-        # Mirrors used in the experiment
-        for smar_name, smar_address in self.smar_config.items():
-            sa = SmarActAxis(self.ID_SA + smar_address)
-            setattr(self, sa, smar_name)
-
-
-    def __repr__(self):
-        ostr = "*****Laser motor positions******\n"
-        for tkey, item in sorted(self.__dict__.items()):
-            if hasattr(item, "get_current_value"):
-                pos = item.get_current_value()
-                posdialstr = ""
-                try:
-                    posdial = item.get_current_value(postype="dial")
-                    posdialstr = "    dial:  % 14g\n" % posdial
-                except:
-                    pass
-                ostr += "  " + tkey.ljust(18) + " : % 14g\n" % pos + posdialstr
-        return ostr
-
-
-
-class Laser_Exp_old:
-
-    def __init__(self, ID=None, name=None, smar_config=None):
-        self.ID = ID
-        self.ID_Exp1 = "SARES20-EXP"
-        self.ID_SA = "SARES23"
-        self.name = name
-        self.smar_config = smar_config
-
-        # Waveplate and Delay stage
-        self.pump_wp = Motor(ID + "-M534:MOT")
-        self.tt_wp = Motor(ID + "-M533:MOT")
-
-        self.pump_delay_stg = Motor(ID + "-M521:MOTOR_1")
-        self.pump_delay = DelayStage(self.pump_delay_stg)
-
-        self.tt_delay_stg = Motor(ID + "-M522:MOTOR_1")
-        self.tt_delay = DelayStage(self.tt_delay_stg)
-#        self.thz_delay = DelayStage(self.thz_delay_stg)
-
-        self.exp_delay_stg = Motor(ID + "-M553:MOT")
-        self.exp_delay = DelayStage(self.exp_delay_stg)
-#        self.thz_delay = DelayStage(self.thz_delay_stg)
-
-        # compressor
-        self.compressor = Motor(ID + "-M532:MOT")
-#        self.compressor = Motor(ID + '-M532:MOT')
-
-        # LAM delay stages
-        self.lam_delay_smar_stg = SmarActAxis("SLAAR21-LMTS-LAM11")
-        self.lam_delay_smar = DelayStage(self.lam_delay_smar_stg)
-#        self.lam_delay_stg_smar = SmarActAxis('SLAAR21-LMTS-LAM11')
-#        self.lam_delay_smar = DelayStage(self.lam_delay_stg_smar)
-
-        self.lam_delay_stg = Motor(ID + "-M548:MOT")
-        self.lam_delay = DelayStage(self.lam_delay_stg)
-#        self.lam_delay_stg = Motor(ID + '-M548:MOT')
-#        self.lam_delay = DelayStage(self.lam_delay_stg)
-
-        # PALM delay stages
-        self.palm_delay_stg = Motor(ID + "-M552:MOT")
-        self.palm_delay = DelayStage(self.palm_delay_stg)
-#        self.palm_delay_stg = Motor(ID + '-M552:MOT')
-#        self.palm_delay = DelayStage(self.palm_delay_stg)
-
-        # PSEN delay stages
-#        self.psen_delay_stg = Motor(ID + '')
-#        self.psen_delay = DelayStage(self.pump_delay_stg)
 
         # SmarActID
         # Mirrors used in the experiment
