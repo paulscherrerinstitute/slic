88aa3c91573a866ed19d22e0fa187db27f1e337d
Sven Augustin <sven.augustin@psi.ch>
2022-07-07 19:24:47 +0200

    cleaned up Alvra XTG

---
+++ slic/devices/endstations/alvra_xtg.py
@@ -0,0 +1,28 @@
+from slic.core.device import Device, SimpleDevice
+from self.devices.general.smaract import SmarActAxis
+
+
+class XTG(Device):
+
+    def __init__(self, ID, name="XTG SmarAct motor positions", **kwargs):
+        super().__init__(ID, name=name, **kwargs)
+
+        self.sample = SimpleDevice("Sample",
+            x = SmarActAxis(ID + ":TRX3"),
+            y = SmarActAxis(ID + ":TRY3")
+        )
+
+        self.grating1 = = SimpleDevice("Grating 1",
+            x = SmarActAxis(ID + ":TRX1"),
+            y = SmarActAxis(ID + ":TRY1"),
+            z = SmarActAxis(ID + ":TRZ1")
+        )
+
+        self.grating2 = = SimpleDevice("Grating 2",
+            x = SmarActAxis(ID + ":TRX2"),
+            y = SmarActAxis(ID + ":TRY2"),
+            z = SmarActAxis(ID + ":TRZ2")
+        )
+
+
+
