81fe9e94311539a35864cdd10d6e85a21f9c2597
Sven Augustin <sven.augustin@psi.ch>
2022-07-07 20:20:00 +0200

    cleaned up XRD

--- slic/devices/endstations/bernina_xrd.py
+++ slic/devices/endstations/bernina_xrd.py
@@ -1,92 +1,73 @@
-from slic.devices.general.motor import Motor
 from slic.core.adjustable import PVAdjustable
-from slic.utils.deprecated.aliases import Alias, append_object_to_object
+from slic.core.device import Device
+from slic.devices.general.motor import Motor
 
 
-class XRD:
+class XRD(Device):
 
-    def __init__(self, name=None, ID=None, configuration=["base"]):
-        """X-ray diffractometer platform in AiwssFEL Bernina.\
-                <configuration> : list of elements mounted on 
-                the plaform, options are kappa, nutable, hlgonio, polana"""
-        self.ID = ID
-        self.name = name
-        self.alias = Alias(name)
+    def __init__(self, ID, name="XRD", configuration=("base",), **kwargs):
+        """
+        X-ray diffractometer platform in SwissFEL Bernina.
+        <configuration> list of elements mounted on the plaform,
+                        options are kappa, nutable, hlgonio, polana
+        """
+        super().__init__(ID, name=name, **kwargs)
         self.configuration = configuration
 
-        if "base" in self.configuration:
-            ### motors base platform ###
-            ### motors base platform ###
-            append_object_to_object(self, Motor, ID + ":MOT_TX", name="xbase")
-            append_object_to_object(self, Motor, ID + ":MOT_TY", name="ybase")
-            append_object_to_object(self, Motor, ID + ":MOT_RX", name="rxbase")
-            append_object_to_object(self, Motor, ID + ":MOT_MY_RYTH", name="alpha")
+        # base platform
+        if "base" in configuration:
+            self.xbase  = Motor(ID + ":MOT_TX")
+            self.ybase  = Motor(ID + ":MOT_TY")
+            self.rxbase = Motor(ID + ":MOT_RX")
+            self.alpha  = Motor(ID + ":MOT_MY_RYTH")
 
-        if "arm" in self.configuration:
-            ### motors XRD detector arm ###
-            append_object_to_object(self, Motor, ID + ":MOT_NY_RY2TH", name="gamma")
-            append_object_to_object(self, Motor, ID + ":MOT_DT_RX2TH", name="delta")
-            ### motors XRD area detector branch ###
-            append_object_to_object(self, Motor, ID + ":MOT_D_T", name="tdet")
-
-            ### motors XRD polarisation analyzer branch ###
-            append_object_to_object(self, Motor, ID + ":MOT_P_T", name="tpol")
+        if "arm" in configuration:
+            # XRD detector arm
+            self.gamma = Motor(ID + ":MOT_NY_RY2TH")
+            self.delta = Motor(ID + ":MOT_DT_RX2TH")
+            # XRD area detector branch
+            self.tdet = Motor(ID + ":MOT_D_T")
+            # XRD polarisation analyzer branch
+            self.tpol = Motor(ID + ":MOT_P_T")
             # missing: slits of flight tube
 
-        if "hlxz" in self.configuration:
-            ### motors heavy load goniometer ###
-            append_object_to_object(self, Motor, ID + ":MOT_TBL_TX", name="xhl")
-            append_object_to_object(self, Motor, ID + ":MOT_TBL_TZ", name="zhl")
-        if "hly" in self.configuration:
-            append_object_to_object(self, Motor, ID + ":MOT_TBL_TY", name="yhl")
+        # nu table
+        if "phi_table" in configuration:
+            self.phi  = Motor(ID + ":MOT_HEX_RX")
+            self.tphi = Motor(ID + ":MOT_HEX_TX")
 
-        if "hlrxrz" in self.configuration:
-            try:
-                append_object_to_object(self, Motor, ID + ":MOT_TBL_RX", name="rxhl")
-            except:
-                print("XRD.rxhl not found")
-                pass
-            try:
-                append_object_to_object(self, Motor, ID + ":MOT_TBL_RY", name="rzhl")
-            except:
-                print("XRD.rzhl not found")
-                pass
+        # PI hexapod
+        if "phi_hex" in configuration:
+            PI_ID = "SARES20-HEX_PI"
+            self.xhex = PVAdjustable(PI_ID + ":SET-POSI-X", PI_ID + ":POSI-X")
+            self.yhex = PVAdjustable(PI_ID + ":SET-POSI-Y", PI_ID + ":POSI-Y")
+            self.zhex = PVAdjustable(PI_ID + ":SET-POSI-Z", PI_ID + ":POSI-Z")
+            self.uhex = PVAdjustable(PI_ID + ":SET-POSI-U", PI_ID + ":POSI-U")
+            self.vhex = PVAdjustable(PI_ID + ":SET-POSI-V", PI_ID + ":POSI-V")
+            self.whex = PVAdjustable(PI_ID + ":SET-POSI-W", PI_ID + ":POSI-W")
 
-        if "phi_table" in self.configuration:
-            ### motors nu table ###
-            append_object_to_object(self, Motor, ID + ":MOT_HEX_TX", name="tphi")
-            append_object_to_object(self, Motor, ID + ":MOT_HEX_RX", name="phi")
+        # heavy load goniometer
+        if "hlxz" in configuration:
+            self.xhl = Motor(ID + ":MOT_TBL_TX")
+            self.zhl = Motor(ID + ":MOT_TBL_TZ")
 
-        if "phi_hex" in self.configuration:
-            ### motors PI hexapod ###
-            append_object_to_object(self, PVAdjustable, "SARES20-HEX_PI:SET-POSI-X", pvname_readback="SARES20-HEX_PI:POSI-X", name="xhex")
-            append_object_to_object(self, PVAdjustable, "SARES20-HEX_PI:SET-POSI-Y", pvname_readback="SARES20-HEX_PI:POSI-Y", name="yhex")
-            append_object_to_object(self, PVAdjustable, "SARES20-HEX_PI:SET-POSI-Z", pvname_readback="SARES20-HEX_PI:POSI-Z", name="zhex")
-            append_object_to_object(self, PVAdjustable, "SARES20-HEX_PI:SET-POSI-U", pvname_readback="SARES20-HEX_PI:POSI-U", name="uhex")
-            append_object_to_object(self, PVAdjustable, "SARES20-HEX_PI:SET-POSI-V", pvname_readback="SARES20-HEX_PI:POSI-V", name="vhex")
-            append_object_to_object(self, PVAdjustable, "SARES20-HEX_PI:SET-POSI-W", pvname_readback="SARES20-HEX_PI:POSI-W", name="whex")
+        if "hly" in configuration:
+            self.yhl = Motor(ID + ":MOT_TBL_TY")
 
-        if "kappa" in self.configuration:
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_KRX", name="eta")
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_KAP", name="kappa")
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_KPH", name="phi")
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_DTY", name="zkap")
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_DTX", name="xkap")
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_DTZ", name="ykap")
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_DRX", name="rxkap")
-            append_object_to_object(self, Motor, "SARES21-XRD:MOT_KAP_DRZ", name="rykap")
+        if "hlrxrz" in configuration:
+            self.rxhl = Motor(ID + ":MOT_TBL_RX")
+            self.rzhl = Motor(ID + ":MOT_TBL_RY")
 
-    def get_adjustable_positions_str(self):
-        ostr = "*****XRD motor positions******\n"
-
-        for tkey, item in self.__dict__.items():
-            if hasattr(item, "get_current_value"):
-                pos = item.get_current_value()
-                ostr += "  " + tkey.ljust(17) + " : % 14g\n" % pos
-        return ostr
-
-    def __repr__(self):
-        return self.get_adjustable_positions_str()
+        if "kappa" in configuration:
+            KAPPA_PREFIX = "SARES21-XRD:MOT_KAP_"
+            self.eta   = Motor(KAPPA_PREFIX + "KRX")
+            self.kappa = Motor(KAPPA_PREFIX + "KAP")
+            self.phi   = Motor(KAPPA_PREFIX + "KPH")
+            self.zkap  = Motor(KAPPA_PREFIX + "DTY")
+            self.xkap  = Motor(KAPPA_PREFIX + "DTX")
+            self.ykap  = Motor(KAPPA_PREFIX + "DTZ")
+            self.rxkap = Motor(KAPPA_PREFIX + "DRX")
+            self.rykap = Motor(KAPPA_PREFIX + "DRZ")
 
 
 
