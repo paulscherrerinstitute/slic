3b9d8bfdcdeb30f7aed963e89f070cdf94c3b509
Sven Augustin <sven.augustin@psi.ch>
2022-07-08 19:38:49 +0200

    flipped 1 and 2

--- slic/devices/xoptics/slits/slits_old-SlitBlades_JJ.py
+++ slic/devices/xoptics/slits/slits_old-SlitBlades_JJ.py
@@ -8,16 +8,16 @@ class SlitBlades_JJ(SlitBase):
     def __init__(self, ID, name=None, elog=None):
         self.ID = ID
         self.name = name
-        self._x1 = Motor(ID + ":MOT2")
-        self._x2 = Motor(ID + ":MOT3")
-        self._y1 = Motor(ID + ":MOT4")
-        self._y2 = Motor(ID + ":MOT5")
+        self._x1 = Motor(ID + ":MOT3")
+        self._x2 = Motor(ID + ":MOT2")
+        self._y1 = Motor(ID + ":MOT5")
+        self._y2 = Motor(ID + ":MOT4")
 
     def get_hg(self):
-        return -(self._x2.get_current_value() - self._x1.get_current_value())
+        return self._x2.get_current_value() - self._x1.get_current_value()
 
     def get_vg(self):
-        return -(self._y2.get_current_value() - self._y1.get_current_value())
+        return self._y2.get_current_value() - self._y1.get_current_value()
 
     def get_ho(self):
         return (self._x1.get_current_value() + self._x2.get_current_value()) / 2
@@ -27,26 +27,26 @@ class SlitBlades_JJ(SlitBase):
 
     def set_hg(self, value):
         ho = self.get_ho()
-        c1 = self._x1.set_target_value(ho + value / 2)
-        c2 = self._x2.set_target_value(ho - value / 2)
+        c1 = self._x1.set_target_value(ho - value / 2)
+        c2 = self._x2.set_target_value(ho + value / 2)
         return c1, c2
 
     def set_vg(self, value):
         vo = self.get_vo()
-        c1 = self._y1.set_target_value(vo + value / 2)
-        c2 = self._y2.set_target_value(vo - value / 2)
+        c1 = self._y1.set_target_value(vo - value / 2)
+        c2 = self._y2.set_target_value(vo + value / 2)
         return c1, c2
 
     def set_ho(self, value):
         hg = self.get_hg()
-        c1 = self._x1.set_target_value(value + hg / 2)
-        c2 = self._x2.set_target_value(value - hg / 2)
+        c1 = self._x1.set_target_value(value - hg / 2)
+        c2 = self._x2.set_target_value(value + hg / 2)
         return c1, c2
 
     def set_vo(self, value):
         vg = self.get_vg()
-        c1 = self._y1.set_target_value(value + vg / 2)
-        c2 = self._y2.set_target_value(value - vg / 2)
+        c1 = self._y1.set_target_value(value - vg / 2)
+        c2 = self._y2.set_target_value(value + vg / 2)
         return c1, c2
 
 
