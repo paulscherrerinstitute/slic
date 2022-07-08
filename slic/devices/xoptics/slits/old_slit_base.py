6b613a01e06be2a3f4b5914f2e7fe201dd03f73e
Sven Augustin <sven.augustin@psi.ch>
2022-07-08 19:34:18 +0200

    DRY

---
+++ slic/devices/xoptics/slits/old_slit_base.py
@@ -0,0 +1,14 @@
+
+class SlitBase:
+
+    def __call__(self, width, height):
+        self.set_hg(width)
+        self.set_vg(height)
+
+    def __repr__(self):
+        string1 = "gap: (%g,%g) mm" % (self.get_hg(), self.get_vg())
+        string2 = "pos: (%g,%g) mm" % (self.get_ho(), self.get_vo())
+        return "\n".join((string1, string2))
+
+
+
