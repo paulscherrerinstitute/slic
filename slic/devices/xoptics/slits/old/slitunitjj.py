7bc352c726175d07b993d6f915ddd1f54026fedd
Sven Augustin <sven.augustin@psi.ch>
2022-07-09 20:15:56 +0200

    SlitUnitJJ forwards kwargs

--- slic/devices/xoptics/slits/old/slitunitjj.py
+++ slic/devices/xoptics/slits/old/slitunitjj.py
@@ -3,12 +3,12 @@ from slitunit import SlitUnit
 
 class SlitUnitJJ(SlitUnit):
 
-    def __init__(self, ID):
+    def __init__(self, ID, **kwargs):
         x1 = ID + ":MOT3"
         x2 = ID + ":MOT2"
         y1 = ID + ":MOT5"
         y2 = ID + ":MOT4"
-        super().__init__(ID, x1, x2, y1, y2)
+        super().__init__(ID, x1, x2, y1, y2, **kwargs)
 
 
 
