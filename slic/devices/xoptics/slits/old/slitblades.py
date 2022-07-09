97ac019f0b99863e9a02384b235cdd0dc9479dd6
Sven Augustin <sven.augustin@psi.ch>
2022-07-09 20:23:34 +0200

    order

--- slic/devices/xoptics/slits/old/slitblades.py
+++ slic/devices/xoptics/slits/old/slitblades.py
@@ -9,7 +9,7 @@ class SlitBlades(Device):
         super().__init__(ID, **kwargs)
         self.mot1 = Motor(mot1)
         self.mot2 = Motor(mot2)
-        self.width, self.center = make_slit_width_center(ID, mot1, mot2)
+        self.center, self.width = make_slit_center_width(ID, mot1, mot2)
 
     @classmethod
     def from_motor_name(cls, ID, motor_name="MOTOR_X"):
@@ -18,38 +18,12 @@ class SlitBlades(Device):
         return cls(ID, mot1, mot2)
 
 
-def make_slit_width_center(ID, mot1, mot2):
-    sw = SlitWidth(ID + "-WIDTH", mot1, mot2)
+def make_slit_center_width(ID, mot1, mot2):
     sc = SlitCenter(ID + "-CENTER", mot1, mot2)
-    sw._center = sc
+    sw = SlitWidth(ID + "-WIDTH", mot1, mot2)
     sc._width = sw
-    return sw, sc
-
-
-class SlitWidth(Adjustable):
-
-    def __init__(self, ID, mot1, mot2, **kwargs):
-        super().__init__(ID, **kwargs)
-        self.mot1 = mot1
-        self.mot2 = mot2
-        self._center = None
-
-    def get_current_value(self):
-        val1 = self.mot1.get_current_value()
-        val2 = self.mot2.get_current_value()
-        return val2 - val1
-
-    def set_target_value(self, value):
-        adj_center = self._center
-        if adj_center is None:
-            raise ValueError("SlitWidth \"{self.ID}\" does not know SlitCenter")
-        center = adj_center.get_current_value()
-        v1 = center - value / 2
-        v2 = center + value / 2
-        t1 = self.mot1.set_target_value(v1)
-        t2 = self.mot2.set_target_value(v2)
-        t1.wait()
-        t2.wait()
+    sw._center = sc
+    return sc, sw
 
 
 class SlitCenter(Adjustable):
@@ -72,6 +46,32 @@ class SlitCenter(Adjustable):
         width = adj_width.get_current_value()
         v1 = value - width / 2
         v2 = value + width / 2
+        t1 = self.mot1.set_target_value(v1)
+        t2 = self.mot2.set_target_value(v2)
+        t1.wait()
+        t2.wait()
+
+
+class SlitWidth(Adjustable):
+
+    def __init__(self, ID, mot1, mot2, **kwargs):
+        super().__init__(ID, **kwargs)
+        self.mot1 = mot1
+        self.mot2 = mot2
+        self._center = None
+
+    def get_current_value(self):
+        val1 = self.mot1.get_current_value()
+        val2 = self.mot2.get_current_value()
+        return val2 - val1
+
+    def set_target_value(self, value):
+        adj_center = self._center
+        if adj_center is None:
+            raise ValueError("SlitWidth \"{self.ID}\" does not know SlitCenter")
+        center = adj_center.get_current_value()
+        v1 = center - value / 2
+        v2 = center + value / 2
         t1 = self.mot1.set_target_value(v1)
         t2 = self.mot2.set_target_value(v2)
         t1.wait()
