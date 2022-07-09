743854d9e958a9301e43e5feacfe7d409f796be7
Sven Augustin <sven.augustin@psi.ch>
2022-07-09 20:15:26 +0200

    rewrote SlitUnit as Device

--- slic/devices/xoptics/slits/old/slitunit.py
+++ slic/devices/xoptics/slits/old/slitunit.py
@@ -1,11 +1,12 @@
-from slitbase import SlitBase
+from slic.core.device import Device
+
 from slitblades import SlitBlades
 
 
-class SlitUnit(SlitBase):
+class SlitUnit(Device):
 
-    def __init__(self, ID, x1, x2, y1, y2):
-        self.ID = ID
+    def __init__(self, ID, x1, x2, y1, y2, **kwargs):
+        super().__init__(ID, **kwargs)
         self.x = SlitBlades(ID + "-X", x1, x2)
         self.y = SlitBlades(ID + "-Y", y1, y2)
 
@@ -16,30 +17,6 @@ class SlitUnit(SlitBase):
         y1 = ID + ":" + motor_name + "Y1"
         y2 = ID + ":" + motor_name + "Y2"
         return cls(ID, x1, x2, y1, y2)
-
-    def get_hg(self):
-        return self.x.get_gap()
-
-    def get_vg(self):
-        return self.y.get_gap
-
-    def get_ho(self):
-        return self.x.get_pos()
-
-    def get_vo(self):
-        return self.y.get_pos()
-
-    def set_hg(self, *args, **kwargs):
-        return self.x.set_gap(*args, **kwargs)
-
-    def set_vg(self, *args, **kwargs):
-        return self.y.set_gap(*args, **kwargs)
-
-    def set_ho(self, *args, **kwargs):
-        return self.x.set_pos(*args, **kwargs)
-
-    def set_vo(self, *args, **kwargs):
-        return self.y.set_pos(*args, **kwargs)
 
 
 
