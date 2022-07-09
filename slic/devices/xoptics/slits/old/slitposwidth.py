a97e92b41a7149c9e41125658a35a6362441676a
Sven Augustin <sven.augustin@psi.ch>
2022-07-09 20:17:23 +0200

    rewrote SlitPosWidth as Device

--- slic/devices/xoptics/slits/old/slitposwidth.py
+++ slic/devices/xoptics/slits/old/slitposwidth.py
@@ -1,40 +1,21 @@
+from slic.core.device import Device, SimpleDevice
 from slic.devices.general.motor import Motor
 
-from slitbase import SlitBase
 
+class SlitPosWidth(Device):
 
-class SlitPosWidth(SlitBase):
+    def __init__(self, ID, **kwargs):
+        super().__init__(ID, **kwargs)
 
-    def __init__(self, ID):
-        self.ID = ID
-        self.hpos = Motor(ID + ":MOTOR_X")
-        self.vpos = Motor(ID + ":MOTOR_Y")
-        self.hgap = Motor(ID + ":MOTOR_W")
-        self.vgap = Motor(ID + ":MOTOR_H")
+        self.x = SimpleDevice(ID + "-X",
+            center = Motor(ID + ":MOTOR_X"),
+            width = Motor(ID + ":MOTOR_W")
+        )
 
-    def get_hg(self):
-        return self.hgap.get_current_value()
-
-    def get_vg(self):
-        return self.vgap.get_current_value()
-
-    def get_ho(self):
-        return self.hpos.get_current_value()
-
-    def get_vo(self):
-        return self.vpos.get_current_value()
-
-    def set_hg(self, value):
-        return self.hgap.set_target_value(value)
-
-    def set_vg(self, value):
-        return self.vgap.set_target_value(value)
-
-    def set_ho(self, value):
-        return self.hpos.set_target_value(value)
-
-    def set_vo(self, value):
-        return self.vpos.set_target_value(value)
+        self.y = SimpleDevice(ID + "-Y",
+            center = Motor(ID + ":MOTOR_Y"),
+            width = Motor(ID + ":MOTOR_H")
+        )
 
 
 
