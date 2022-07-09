5b9b2b7f46c472eb827a37175111cfb244fa8622
Sven Augustin <sven.augustin@psi.ch>
2022-07-09 17:19:33 +0200

    consistent names

--- slic/devices/xoptics/slits/old/slitposwidth.py
+++ slic/devices/xoptics/slits/old/slitposwidth.py
@@ -7,34 +7,34 @@ class SlitPosWidth(SlitBase):
 
     def __init__(self, ID):
         self.ID = ID
-        self.xoffs = Motor(ID + ":MOTOR_X")
-        self.yoffs = Motor(ID + ":MOTOR_Y")
-        self.xgap  = Motor(ID + ":MOTOR_W")
-        self.ygap  = Motor(ID + ":MOTOR_H")
+        self.hpos = Motor(ID + ":MOTOR_X")
+        self.vpos = Motor(ID + ":MOTOR_Y")
+        self.hgap = Motor(ID + ":MOTOR_W")
+        self.vgap = Motor(ID + ":MOTOR_H")
 
     def get_hg(self):
-        return self.xgap.get_current_value()
+        return self.hgap.get_current_value()
 
     def get_vg(self):
-        return self.ygap.get_current_value()
+        return self.vgap.get_current_value()
 
     def get_ho(self):
-        return self.xoffs.get_current_value()
+        return self.hpos.get_current_value()
 
     def get_vo(self):
-        return self.yoffs.get_current_value()
+        return self.vpos.get_current_value()
 
     def set_hg(self, value):
-        return self.xgap.set_target_value(value)
+        return self.hgap.set_target_value(value)
 
     def set_vg(self, value):
-        return self.ygap.set_target_value(value)
+        return self.vgap.set_target_value(value)
 
     def set_ho(self, value):
-        return self.xoffs.set_target_value(value)
+        return self.hpos.set_target_value(value)
 
     def set_vo(self, value):
-        return self.yoffs.set_target_value(value)
+        return self.vpos.set_target_value(value)
 
 
 
