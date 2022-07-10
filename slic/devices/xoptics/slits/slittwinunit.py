0b901582da53a0e2bc270692baee5c05537199d5
Sven Augustin <sven.augustin@psi.ch>
2022-07-10 14:00:52 +0200

    added combined adjustable to set center/width for both slit units simultaneously

--- slic/devices/xoptics/slits/slittwinunit.py
+++ slic/devices/xoptics/slits/slittwinunit.py
@@ -1,4 +1,5 @@
-from slic.core.device import Device
+from slic.core.adjustable import Combined
+from slic.core.device import Device, SimpleDevice
 
 from slitunit import SlitUnit
 
@@ -7,8 +8,20 @@ class SlitTwinUnit(Device):
 
     def __init__(self, ID, **kwargs):
         super().__init__(ID, **kwargs)
-        self.a = SlitUnit.from_motor_name(ID, "MOTOR_A")
-        self.b = SlitUnit.from_motor_name(ID, "MOTOR_B")
+
+        self.a = a = SlitUnit.from_motor_name(ID, "MOTOR_A")
+        self.b = b = SlitUnit.from_motor_name(ID, "MOTOR_B")
+
+        self.x = make_combined(ID + "-X", a.x, b.x)
+        self.y = make_combined(ID + "-Y", a.y, b.y)
+
+
+
+def make_combined(ID, d1, d2):
+    return SimpleDevice(ID,
+        center = Combined(ID + "-CENTER", (d1.center, d2.center)),
+        width = Combined(ID + "-WIDTH", (d1.width, d2.width))
+    )
 
 
 
