370913390a5c850d325c884f9cc3109659377a05
Sven Augustin <sven.augustin@psi.ch>
2022-07-11 11:47:51 +0200

    rewrote KBs as devices

--- slic/devices/xoptics/kbver.py
+++ slic/devices/xoptics/kbver.py
@@ -1,39 +1,30 @@
-#from slic.utils.hastyepics import get_pv as PV
+from slic.core.adjustable import PVEnumAdjustable
+from slic.core.device import Device
 from slic.devices.general.motor import Motor
 
 
-class KBver:
+class KBVer(Device):
 
-    def __init__(self, ID, z_undulator=None, description=None):
-        self.ID = ID
+    def __init__(self, ID, name="Vertical KB mirror", **kwargs):
+        super().__init__(ID, name=name, **kwargs)
+
+#        self.mode = PVEnumAdjustable(ID[:11] + ":MODE")
 
         self.x = Motor(ID + ":W_X")
         self.y = Motor(ID + ":W_Y")
+
         self.pitch = Motor(ID + ":W_RX")
-        self.roll = Motor(ID + ":W_RZ")
-        self.yaw = Motor(ID + ":W_RY")
+        self.roll  = Motor(ID + ":W_RZ")
+        self.yaw   = Motor(ID + ":W_RY")
+
         self.bend1 = Motor(ID + ":BU")
         self.bend2 = Motor(ID + ":BD")
 
-#        self.mode = PV(ID[:11] + ":MODE").enum_strs[PV(ID[:11] + ":MODE").value]
-
-        #### actual motors ###
-        self._Y1 = Motor(ID + ":TY1")
-        self._Y2 = Motor(ID + ":TY2")
-        self._Y3 = Motor(ID + ":TY3")
-        self._X1 = Motor(ID + ":TX1")
-        self._X2 = Motor(ID + ":TX2")
-
-    def __repr__(self):
-        s = "**Vertical KB mirror**\n"
-        motors = "bend1 bend2 pitch roll yaw x y".split()
-        for motor in motors:
-            s += " - %s = %.4f\n" % (motor, getattr(self, motor).wm())
-        s += "\n**Stages**\n"
-        stages = "_Y1 _Y2 _Y3 _X1 _X2".split()
-        for stage in stages:
-            s += " - %s = %.4f\n" % (stage, getattr(self, stage).wm())
-        return s
+        self.Y1 = Motor(ID + ":TY1")
+        self.Y2 = Motor(ID + ":TY2")
+        self.Y3 = Motor(ID + ":TY3")
+        self.X1 = Motor(ID + ":TX1")
+        self.X2 = Motor(ID + ":TX2")
 
 
 
