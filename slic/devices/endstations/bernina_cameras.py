17e40fcee61878912c4475d9ef725228c39e853d
Sven Augustin <sven.augustin@psi.ch>
2022-07-12 12:36:12 +0200

    moved bernina_xeye to bernina_cameras

--- slic/devices/endstations/bernina_cameras.py
+++ slic/devices/endstations/bernina_cameras.py
@@ -6,8 +6,8 @@ from slic.devices.general.detectors import CameraCA, CameraBS
 
 class CameraSigma(Device):
 
-    def __init__(self, zoomstage_pvs=None, camera_pv=None, bshost=None, bsport=None, name="Sigma objective", **kwargs):
-        super().__init__("SIGMA", name=name, **kwargs)
+    def __init__(self, ID="SIGMA", zoomstage_pvs=None, camera_pv=None, bshost=None, bsport=None, name="Sigma objective", **kwargs):
+        super().__init__(ID, name=name, **kwargs)
 
         if zoomstage_pvs:
             self.zoom = PVAdjustable(zoomstage_pvs["set_value"], zoomstage_pvs["readback"])
@@ -19,16 +19,31 @@ class CameraSigma(Device):
             self.camBS = CameraBS(host=bshost, port=bsport)
 
 
-class CameraQioptiq:
+class CameraQioptiq(Device):
 
-    def __init__(self, zoomstage_pv=None, focusstage_pv=None, camera_pv=None, bshost=None, bsport=None, name="Qioptic sample viewer", **kwargs):
-        super().__init__("QIOPTIQ", name=name, **kwargs)
+    def __init__(self, ID="QIOPTIQ", zoomstage_pv=None, focusstage_pv=None, camera_pv=None, bshost=None, bsport=None, name="Qioptic sample viewer", **kwargs):
+        super().__init__(ID, name=name, **kwargs)
 
         if zoomstage_pv:
             self.zoom = Motor(zoomstage_pv)
 
         if focusstage_pv:
             self.focus = Motor(focusstage_pv)
+
+        if camera_pv:
+            self.cam = CameraCA(camera_pv)
+
+        if bshost:
+            self.camBS = CameraBS(host=bshost, port=bsport)
+
+
+class XEye(Device):
+
+    def __init__(self, ID="XEYE", zoomstage_pv=None, camera_pv=None, bshost=None, bsport=None, name="XEye", **kwargs):
+        super().__init__(ID, name=name, **kwargs)
+
+        if zoomstage_pv:
+            self.zoom = Motor(zoomstage_pv)
 
         if camera_pv:
             self.cam = CameraCA(camera_pv)
