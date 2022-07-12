5298d9c22c27bcd234b041b4a35f53ca1185dd5e
Sven Augustin <sven.augustin@psi.ch>
2022-07-12 10:47:04 +0200

    rewrote Attenuator as device: split out Transmission adjustable (covers 1st and 3rd harmonic); split out LimitedEnergy and Foils; motors are moved into separate device

--- slic/devices/xoptics/aramis_attenuator.py
+++ slic/devices/xoptics/aramis_attenuator.py
@@ -1,81 +1,142 @@
 from time import sleep
-from slic.core.adjustable import Adjustable
+
+from slic.core.adjustable import Adjustable, PVAdjustable
+from slic.core.device import Device, SimpleDevice
 from slic.devices.general.motor import Motor
 from slic.utils.hastyepics import get_pv as PV
+from slic.utils.printing import format_header
 
 
-class Attenuator(Adjustable):
+class Attenuator(Device):
 
-    def __init__(self, ID, E_min=1500, sleeptime=1, limits=[-52, 2], pulse_picker=None, name="Attenuator Aramis"):
+    def __init__(self, ID, E_min=1500, process_time=1, mot_limits=(-52, 2), name="Attenuator Aramis"):
         super().__init__(ID, name=name)
 
-        self._pv_status_str = PV(ID + ":MOT2TRANS.VALD")
-        self._pv_status_int = PV(ID + ":IDX_RB")
-
         self.E_min = E_min
-        self._sleeptime = sleeptime
-        self.pulse_picker = pulse_picker
+        self.process_time = process_time
+        self.mot_limits = mot_limits
 
-        self.motors = [Motor(f"{self.ID}:MOTOR_{n+1}", name=f"motor{n+1}") for n in range(6)]
-        for n, mot in enumerate(self.motors):
-            self.__dict__[f"motor_{n+1}"] = mot
-            if limits:
-                mot.set_epics_limits(*limits)
+        motors = make_motors_with_limits(ID, mot_limits)
+        self.motors = SimpleDevice(ID+"-MOTORS", **motors)
 
+        self.trans1st = Transmission(ID, motors.values(), process_time=process_time)
+        self.trans3rd = Transmission(ID, motors.values(), process_time=process_time, third_order=True)
+        self.energy = LimitedEnergy(ID, E_min, process_time)
+        self.foils = Foils(ID)
 
-    def updateE(self, energy=None):
-        while not energy:
-            energy = PV("SARUN03-UIND030:FELPHOTENE").value
-            energy = energy * 1000
-            if energy < self.E_min:
-                energy = None
-                print(f"Machine photon energy is below {self.E_min} - waiting for the machine to recover")
-                sleep(self._sleeptime)
-        PV(self.ID + ":ENERGY").put(energy)
-        print("Set energy to %s eV" % energy)
-
-    def set_transmission(self, value, energy=None):
-        self.updateE(energy)
-        PV(self.ID + ":3RD_HARM_SP").put(0)
-        PV(self.ID + ":TRANS_SP").put(value)
-
-    def set_transmission_third_harmonic(self, value, energy=None):
-        self.updateE(energy)
-        PV(self.ID + ":3RD_HARM_SP").put(1)
-        PV(self.ID + ":TRANS_SP").put(value)
-
-    def get_transmission(self, verbose=True):
-        tFun = PV(self.ID + ":TRANS_RB").value
-        tTHG = PV(self.ID + ":TRANS3EDHARM_RB").value
-        if verbose:
-            print("Transmission Fundamental: %s THG: %s" % (tFun, tTHG))
-        return tFun, tTHG
-
-    def get_current_value(self, *args, **kwargs):
-        return self.get_transmission(*args, verbose=False, **kwargs)[0]
-
-    def set_target_value(self, value):
-        self.set_transmission(value)
-        sleep(self.sleeptime)
-
-    def get_status(self):
-        s_str = self._pv_status_str.get(as_string=True)
-        s_int = self._pv_status_int.get()
-        return s_str, s_int
 
     def __repr__(self):
-        t = self.get_transmission(verbose=False)
-        s = "1st harm. transmission\t=  %g\n" % t[0]
-        s += "3rd harm. transmission\t=  %g\n" % t[1]
-        s += "Targets in beam:\n"
-        s += "%s" % self.get_status()[0]
-        return s
+        res = super().__repr__()
+        return res + "\n" + str(self.foils)
 
-    def __call__(self, *args, **kwargs):
-        self.set_transmission(*args, **kwargs)
+
+    def get_transmission(self):
+        return self.trans1st.get_current_value()
+
+    def get_transmission_third_harmonic(self):
+        return self.trans3rd.get_current_value()
+
+    def set_transmission(self, value, energy=None):
+        self.energy.set(energy)
+        self.trans1st.get_current_value(value).wait()
+
+    def set_transmission_third_harmonic(self, value, energy=None):
+        self.energy.set(energy)
+        self.trans3rd.get_current_value(value).wait()
+
+
+
+def make_motors_with_limits(ID, limits, n=6):
+    motors = {f"m{i+1}": Motor(f"{ID}:MOTOR_{i+1}") for i in range(n)}
+    if limits:
+        for m in motors.values():
+            m.set_epics_limits(*limits)
+    return motors
+
+
+
+class Transmission(PVAdjustable):
+
+    def __init__(self, ID, motors, third_order=False, **kwargs):
+        self.motors = motors
+        self.third_order = third_order
+
+        prefix = ID + ":"
+        pvname_setvalue = prefix + "TRANS_SP"
+        pvname_readback = prefix + ("TRANS3EDHARM_RB" if third_order else "TRANS_RB")
+        super().__init__(pvname_setvalue, pvname_readback, **kwargs)
+
+        self.pvnames.third_order_toggle = pvn = prefix + "3RD_HARM_SP"
+        self.pvs.third_order_toggle = PV(pvn)
+
+
+    def set_target_value(self, *args, **kwargs):
+        self.set_third_order_toggle()
+        super().set_target_value(*args, **kwargs)
+
+    def set_third_order_toggle(self):
+        self.pvs.third_order_toggle.put(self.third_order)
 
     def is_moving(self):
-        raise NotImplementedError
+        return any(m.is_moving() for m in self.motors)
+
+
+
+class LimitedEnergy:
+
+    def __init__(self, ID, E_min, wait_time):
+        self.E_min = E_min
+        self.wait_time = wait_time
+
+        self.pv_att_energy = PV(ID + ":ENERGY")
+        self.pv_fel_energy = PV("SARUN03-UIND030:FELPHOTENE")
+
+
+    def set(self, energy=None):
+        E_min = self.E_min
+        wait_time = self.wait_time
+
+        while not energy:
+            energy = self.pv_fel_energy.get()
+            if energy is not None:
+                energy *= 1000
+                if energy >= E_min:
+                    break
+
+            print(f"Machine photon energy ({energy} eV) is below {E_min} eV - waiting for the machine to recover...")
+            energy = None
+            sleep(wait_time)
+
+        self.pv_att_energy.put(energy)
+        print(f"Set attenuator energy to {energy} eV")
+
+
+
+class Foils:
+
+    def __init__(self, ID):
+        self.pv_names = PV(ID + ":MOT2TRANS.VALD")
+        self.pv_index = PV(ID + ":IDX_RB")
+
+    def __repr__(self):
+        header = format_header("Selected Foils", "=")
+        printable = " | ".join(str(i) for i in self.get())
+        printable = "| " + printable + " |"
+        return header + "\n" + printable
+
+    def get(self):
+        names = self.get_names()
+        index = self.get_index()
+        return [None if n is None else f"{n} ({i})" for n, i in zip(names, index)]
+
+    def get_names(self):
+        return list(range(10))
+        names = self.pv_names.get(as_string=True).strip().split()
+        return [None if n == "None" else n for n in names]
+
+    def get_index(self):
+        return list(range(10))
+        return self.pv_index.get().tolist()
 
 
 
