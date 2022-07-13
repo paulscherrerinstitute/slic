c7082a335d8b37c076840ab366942a2cc5cbd663
Sven Augustin <sven.augustin@psi.ch>
2022-07-13 11:40:16 +0200

    rewrote SolidTargetDetectorPBPS as device; split out DiodeArray, Calibrator, CalcIntensity

--- slic/devices/xdiagnostics/intensitymonitor.py
+++ slic/devices/xdiagnostics/intensitymonitor.py
@@ -1,164 +1,197 @@
+from collections import defaultdict
 import numpy as np
 
 from slic.core.adjustable import PVEnumAdjustable
+from slic.core.device import Device
 from slic.devices.general.detectors_new import FeDigitizer, PvDataStream
 from slic.devices.general.motor import Motor
 from slic.utils.hastyepics import get_pv as PV
+from slic.utils.printing import format_header, printable_dict_of_dicts
 
 
-class SolidTargetDetectorPBPS:
+class SolidTargetDetectorPBPS(Device):
 
     def __init__(self, ID,
-        VME_crate=None, link=None, ch_up=12, ch_down=13, ch_right=14, ch_left=15,
-        channels=None,
-        calc=None,
-        name=None
+        vme_crate=None, link=9, ch_up=12, ch_down=13, ch_right=14, ch_left=15, # -> DiodeArray
+        calib_channels=None, # -> Calibrator
+        calc_channels=None, # -> CalcIntensity
+        name="Intensity Monitor",
+        **kwargs
     ):
-        self.ID = ID
-        self.name = name
-
-        self.diode_x    = Motor(ID + ":MOTOR_X1",    name="diode x")
-        self.diode_y    = Motor(ID + ":MOTOR_Y1",    name="diode y")
-        self.target_pos = Motor(ID + ":MOTOR_PROBE", name="target pos")
+        super().__init__(ID, name=name, **kwargs)
 
         self.target = PVEnumAdjustable(ID + ":PROBE_SP", name="target")
+        self.target_pos = Motor(ID + ":MOTOR_PROBE", name="target pos")
 
-        if VME_crate:
-            self.diode_up    = FeDigitizer("%s:Lnk%dCh%d" % (VME_crate, link, ch_up))
-            self.diode_down  = FeDigitizer("%s:Lnk%dCh%d" % (VME_crate, link, ch_down))
-            self.diode_left  = FeDigitizer("%s:Lnk%dCh%d" % (VME_crate, link, ch_left))
-            self.diode_right = FeDigitizer("%s:Lnk%dCh%d" % (VME_crate, link, ch_right))
-
-        if channels:
-            self.signal_up    = PvDataStream(channels["up"])
-            self.signal_down  = PvDataStream(channels["down"])
-            self.signal_left  = PvDataStream(channels["left"])
-            self.signal_right = PvDataStream(channels["right"])
-
-        if calc:
-            self.intensity = PvDataStream(calc["itot"])
-            self.xpos      = PvDataStream(calc["xpos"])
-            self.ypos      = PvDataStream(calc["ypos"])
-
-
-    def get_calibration_values(self, seconds=5):
-        self.diode_x.set_target_value(0).wait()
-        self.diode_y.set_target_value(0).wait()
-        ds = [self.signal_up, self.signal_down, self.signal_left, self.signal_right]
-        aqs = [d.acquire(seconds=seconds) for d in ds]
-        data = [aq.wait() for aq in aqs]
-        mean = [np.mean(td) for td in data]
-        std = [np.std(td) for td in data]
-        norm_diodes = [1 / tm / 4 for tm in mean]
-        return norm_diodes
-
-    def set_calibration_values(self, norm_diodes):
-        #this is now only for bernina when using the ioxos from sla
-        channels = ["SLAAR21-LTIM01-EVR0:CALCI.INPG", "SLAAR21-LTIM01-EVR0:CALCI.INPH", "SLAAR21-LTIM01-EVR0:CALCI.INPF", "SLAAR21-LTIM01-EVR0:CALCI.INPE"]
-        for tc, tv in zip(channels, norm_diodes):
-            PV(tc).put(bytes(str(tv), "utf8"))
-        channels = ["SLAAR21-LTIM01-EVR0:CALCX.INPE", "SLAAR21-LTIM01-EVR0:CALCX.INPF"]
-        for tc, tv in zip(channels, norm_diodes[2:4]):
-            PV(tc).put(bytes(str(tv), "utf8"))
-        channels = ["SLAAR21-LTIM01-EVR0:CALCY.INPE", "SLAAR21-LTIM01-EVR0:CALCY.INPF"]
-        for tc, tv in zip(channels, norm_diodes[0:2]):
-            PV(tc).put(bytes(str(tv), "utf8"))
-
-    def get_calibration_values_position(self, calib_intensities, seconds=5, motion_range=0.2):
-        self.diode_x.set_epics_limits(-motion_range / 2 - 0.1, +motion_range / 2 + 0.1)
-        self.diode_y.set_epics_limits(-motion_range / 2 - 0.1, +motion_range / 2 + 0.1)
-        self.diode_x.set_target_value(0).wait()
-        self.diode_y.set_target_value(0).wait()
-        raw = []
-        for pos in [motion_range / 2, -motion_range / 2]:
-            self.diode_x.set_target_value(pos).wait()
-            aqs = [ts.acquire(seconds=seconds) for ts in [self.signal_left, self.signal_right]]
-            vals = [np.mean(aq.wait()) * calib for aq, calib in zip(aqs, calib_intensities[0:2])]
-            raw.append((vals[0] - vals[1]) / (vals[0] + vals[1]))
-        grad = motion_range / np.diff(raw)[0]
-        # xcalib = [np.diff(calib_intensities[0:2])[0]/np.sum(calib_intensities[0:2]), grad]
-        xcalib = [0, grad]
-        self.diode_x.set_target_value(0).wait()
-        raw = []
-        for pos in [motion_range / 2, -motion_range / 2]:
-            self.diode_y.set_target_value(pos).wait()
-            aqs = [ts.acquire(seconds=seconds) for ts in [self.signal_up, self.signal_down]]
-            vals = [np.mean(aq.wait()) * calib for aq, calib in zip(aqs, calib_intensities[2:4])]
-            raw.append((vals[0] - vals[1]) / (vals[0] + vals[1]))
-        grad = motion_range / np.diff(raw)[0]
-        # ycalib = [np.diff(calib_intensities[2:4])[0]/np.sum(calib_intensities[2:4]), grad]
-        ycalib = [0, grad]
-        self.diode_y.set_target_value(0).wait()
-        return xcalib, ycalib
-
-    def set_calibration_values_position(self, xcalib, ycalib):
-        channels = ["SLAAR21-LTIM01-EVR0:CALCX.INPJ", "SLAAR21-LTIM01-EVR0:CALCX.INPI"]
-        # txcalib = [-1*xcalib[0],-1*xcalib[1]]
-        for tc, tv in zip(channels, xcalib):
-            PV(tc).put(bytes(str(tv), "utf8"))
-        channels = ["SLAAR21-LTIM01-EVR0:CALCY.INPJ", "SLAAR21-LTIM01-EVR0:CALCY.INPI"]
-        for tc, tv in zip(channels, ycalib):
-            PV(tc).put(bytes(str(tv), "utf8"))
-
-    def calibrate(self, seconds=5):
-        c = self.get_calibration_values(seconds=seconds)
-        self.set_calibration_values(c)
-        xc, yc = self.get_calibration_values_position(c, seconds=seconds)
-        self.set_calibration_values_position(xc, yc)
+        self.diodes = DiodeArray(vme_crate, link, ch_up, ch_down, ch_right, ch_left) if vme_crate else None
+        self.calib  = Calibrator(ID, calib_channels) if calib_channels else None
+        self.calc   = CalcIntensity(calc_channels) if calc_channels else None
 
 
     def __repr__(self):
-        s = f"**Intensity  monitor {self.name}**\n\n"
+        res = super().__repr__()
+        if self.diodes:
+            res += "\n" + repr(self.diodes)
+        return res
 
-        s += f"Target in: {self.target.get_current_value()}\n\n"
-        try:
-            sd = "**Bias voltage**\n"
-            sd += " - Diode up: %.4f\n" % (self.diode_up.get_bias())
-            sd += " - Diode down: %.4f\n" % (self.diode_down.get_bias())
-            sd += " - Diode left: %.4f\n" % (self.diode_left.get_bias())
-            sd += " - Diode right: %.4f\n" % (self.diode_right.get_bias())
-            sd += "\n"
 
-            sd += "**Gain**\n"
-            sd += " - Diode up: %i\n" % (self.diode_up.gain.get())
-            sd += " - Diode down: %i\n" % (self.diode_down.gain.get())
-            sd += " - Diode left: %i\n" % (self.diode_left.gain.get())
-            sd += " - Diode right: %i\n" % (self.diode_right.gain.get())
-            s += sd
-        except:
-            pass
-        return s
 
-    def set_gains(self, value):
-        try:
-            self.diode_up.gain.set(value)
-            self.diode_down.gain.set(value)
-            self.diode_left.gain.set(value)
-            self.diode_right.gain.set(value)
-        except:
-            print("No diodes configured, can not change any gain!")
+class DiodeArray:
+
+    def __init__(self, vme_crate, link, ch_up, ch_down, ch_right, ch_left):
+        prefix = f"{vme_crate}:Lnk{link}Ch"
+        self.up    = up    = FeDigitizer(f"{prefix}{ch_up}")
+        self.down  = down  = FeDigitizer(f"{prefix}{ch_down}")
+        self.left  = left  = FeDigitizer(f"{prefix}{ch_left}")
+        self.right = right = FeDigitizer(f"{prefix}{ch_right}")
+        self._diodes = dict(up=up, down=down, left=left, right=right)
+
+    def __repr__(self):
+        header = format_header("Diode Array", "=")
+        res = defaultdict(dict)
+        for name, diode in self._diodes.items():
+            res["Bias voltage"][name] = diode.get_bias()
+            res["Gain"][name] = diode.gain.get()
+        res = printable_dict_of_dicts(res, sorter=lambda x: list(x))
+        return header + "\n\n" + res
 
     def get_available_gains(self):
-        try:
-            nu = self.diode_up.gain.names
-            nd = self.diode_down.gain.names
-            nl = self.diode_left.gain.names
-            nr = self.diode_right.gain.names
-            assert nu == nd == nl == nr, "NB: the gain options of the four diodes are not equal!!!"
-            return nu
-        except:
-            print("No diodes configured, can not change any gain!")
+        all_names = set(diode.gain.names for diode in self._diodes.values())
+        assert len(all_names) == 1, "gain options for the four diodes are not identical"
+        return all_names.pop()
 
     def get_gains(self):
-        try:
-            gains = dict()
-            gains["up"]    = (self.diode_up.gain.get_name(),    self.diode_up.gain.get())
-            gains["down"]  = (self.diode_down.gain.get_name(),  self.diode_down.gain.get())
-            gains["left"]  = (self.diode_left.gain.get_name(),  self.diode_left.gain.get())
-            gains["right"] = (self.diode_right.gain.get_name(), self.diode_right.gain.get())
-            return gains
-        except:
-            print("No diodes configured, can not change any gain!")
+        res = {}
+        for diode_name, diode in self._diodes.items():
+            gain_name = diode.gain.get_name()
+            gain = diode.gain.get()
+            res[diode_name] = (gain_name, gain)
+        return res
+
+    def set_gains(self, value):
+        for diode in self._diodes.values():
+            diode.gain.set(value)
+
+
+
+class Calibrator(Device):
+
+    def __init__(self, ID, channels):
+        super().__init__(ID + "-CALIB")
+
+        self.diode_x = x = Motor(ID + ":MOTOR_X1", name="diode x")
+        self.diode_y = y = Motor(ID + ":MOTOR_Y1", name="diode y")
+        self._motors = dict(x=x, y=y)
+
+        self.signal_up    = up    = PvDataStream(channels["up"])
+        self.signal_down  = down  = PvDataStream(channels["down"])
+        self.signal_left  = left  = PvDataStream(channels["left"])
+        self.signal_right = right = PvDataStream(channels["right"])
+        self._signals = dict(up=up, down=down, left=left, right=right)
+
+
+    def calibrate(self, wait_time=5):
+        ci = self.get_calibration_intensity(wait_time=wait_time)
+        self.set_calibration_intensity(ci)
+        cx, cy = self.get_calibration_position(ci, wait_time=wait_time)
+        self.set_calibration_position(cx, cy)
+        return ci, cx, cy
+
+    def get_calibration_intensity(self, wait_time=5):
+        self._reset_motors()
+        return calculate_calibration_intensity(self._signals.values(), wait_time)
+
+    def get_calibration_position(self, calib_intensities, motion_range=0.2, wait_time=5):
+        self._set_motor_limits(motion_range)
+        self._reset_motors()
+        cx = calculate_calibration_position(self.diode_x, self.signal_left, self.signal_right, calib_intensities[:2], motion_range, wait_time)
+        cy = calculate_calibration_position(self.diode_y, self.signal_up,   self.signal_down,  calib_intensities[2:], motion_range, wait_time)
+        return cx, cy
+
+
+    def set_calibration_intensity(self, calib_intensities):
+        # this list is for bernina when using the SLA ioxos
+        chs = [
+            "SLAAR21-LTIM01-EVR0:CALCI.INPG",
+            "SLAAR21-LTIM01-EVR0:CALCI.INPH",
+            "SLAAR21-LTIM01-EVR0:CALCI.INPF",
+            "SLAAR21-LTIM01-EVR0:CALCI.INPE",
+
+            "SLAAR21-LTIM01-EVR0:CALCY.INPE",
+            "SLAAR21-LTIM01-EVR0:CALCY.INPF",
+
+            "SLAAR21-LTIM01-EVR0:CALCX.INPE",
+            "SLAAR21-LTIM01-EVR0:CALCX.INPF"
+
+        ]
+        # CALCI gets each norms values, CALCY gets the first two, CALCX gets the last two
+        vals = list(calib_intensities) * 2
+        fill_channels(chs, vals)
+
+
+    def set_calibration_position(self, calib_pos_x, calib_pos_y):
+        chs = [
+            "SLAAR21-LTIM01-EVR0:CALCX.INPJ",
+            "SLAAR21-LTIM01-EVR0:CALCX.INPI",
+
+            "SLAAR21-LTIM01-EVR0:CALCY.INPJ",
+            "SLAAR21-LTIM01-EVR0:CALCY.INPI"
+        ]
+        vals = list(calib_pos_x) + list(calib_pos_y)
+        fill_channels(chs, vals)
+
+
+    def _reset_motors(self):
+        tasks = [m.set_target_value(0) for m in self._motors.values()]
+        for t in tasks:
+            t.wait()
+
+    def _set_motor_limits(self, motion_range):
+        limit = motion_range / 2 + 0.1
+        for m in self._motors.values():
+            m.set_epics_limits(-limit, +limit)
+
+
+
+def calculate_calibration_intensity(sigs, wait_time):
+    acqs = [d.acquire(seconds=wait_time) for d in sigs]
+    data = [a.wait() for a in acqs]
+    means = [np.mean(d) for d in data]
+#    stds = [np.std(d) for d in data]
+    calib = [1 / m / 4 for m in means]
+    return calib
+
+
+def calculate_calibration_position(mot, sig1, sig2, calib_intensities, motion_range, wait_time):
+    border = motion_range / 2
+    raw = []
+    for pos in (-border, +border):
+        mot.set_target_value(pos).wait()
+        acqs = [s.acquire(seconds=wait_time) for s in (sig1, sig2)]
+        v0, v1 = [np.mean(a.wait()) * c for a, c in zip(acqs, calib_intensities)]
+        delta  = v0 - v1
+        summed = v0 + v1
+        res = delta / summed
+        raw.append(res)
+
+    grad = motion_range / np.diff(raw)[0]
+#    calib = [np.diff(calib_intensities)[0] / np.sum(calib_intensities), grad]
+    calib = [0, grad]
+    return calib
+
+
+def fill_channels(chs, vals):
+    for c, v in zip(chs, vals):
+        PV(c).put(bytes(str(v), "utf8")) #TODO: this is probably not a good idea
+
+
+
+class CalcIntensity:
+
+    def __init__(self, channels):
+        self.itot = PvDataStream(channels["itot"])
+        self.xpos = PvDataStream(channels["xpos"])
+        self.ypos = PvDataStream(channels["ypos"])
 
 
 
