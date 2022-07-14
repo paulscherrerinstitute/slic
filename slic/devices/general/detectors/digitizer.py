8b4a7420d78877bf53ecbff3210edb96fe304ee7
Sven Augustin <sven.augustin@psi.ch>
2022-07-14 17:31:50 +0200

    rewrote digitizers as devices

--- slic/devices/general/detectors/digitizer.py
+++ slic/devices/general/detectors/digitizer.py
@@ -1,32 +1,31 @@
-class FeDigitizer:
-
-    def __init__(self, ID, elog=None):
-        self.ID = ID
-        self.gain = EnumWrapper(ID + "-WD-gain")
-        self._bias = PV(ID + "-HV_SET")
-        self.channels = [
-            ID + "-BG-DATA",
-            ID + "-BG-DRS_TC",
-            ID + "-BG-PULSEID-valid",
-            ID + "-DATA",
-            ID + "-DRS_TC",
-            ID + "-PULSEID-valid"
-        ]
-
-    def set_bias(self, value):
-        self._bias.put(value)
-
-    def get_bias(self):
-        return self._bias.value
+from slic.core.adjustable import PVAdjustable, PVEnumAdjustable
+from slic.core.device import Device
 
 
-class DiodeDigitizer:
+class FeDigitizer(Device):
 
-    def __init__(self, ID, VME_crate=None, link=None, ch_0=7, ch_1=8, elog=None):
-        self.ID = ID
-        if VME_crate:
-            self.diode_0 = FeDigitizer("%s:Lnk%dCh%d" % (VME_crate, link, ch_0))
-            self.diode_1 = FeDigitizer("%s:Lnk%dCh%d" % (VME_crate, link, ch_1))
+    def __init__(self, ID, **kwargs):
+        super().__init__(ID, **kwargs)
+        self.bias = PVAdjustable(ID + "-HV_SET")
+        self.gain = PVEnumAdjustable(ID + "-WD-gain")
+
+#        self.channels = [
+#            ID + "-BG-DATA",
+#            ID + "-BG-DRS_TC",
+#            ID + "-BG-PULSEID-valid",
+#            ID + "-DATA",
+#            ID + "-DRS_TC",
+#            ID + "-PULSEID-valid"
+#        ]
+
+
+class DiodeDigitizer(Device):
+
+    def __init__(self, ID, vme_crate, link=9, ch0=7, ch1=8, **kwargs):
+        super().__init__(ID, **kwargs)
+        prefix = f"{vme_crate}:Lnk{link}Ch"
+        self.diode0 = FeDigitizer(f"{prefix}{ch0}")
+        self.diode1 = FeDigitizer(f"{prefix}{ch1}")
 
 
 
