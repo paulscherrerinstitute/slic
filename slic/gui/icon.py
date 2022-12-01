743025755051a50b30827e754c037f60a7264851
Sven Augustin <sven.augustin@psi.ch>
2022-12-01 12:24:52 +0100

    split out icon path function

--- slic/gui/icon.py
+++ slic/gui/icon.py
@@ -3,9 +3,12 @@ import wx
 
 
 def get_wx_icon(fname="icon.png"):
-    iname = os.path.dirname(__file__)
-    iname = os.path.join(iname, fname)
+    iname = get_icon_path(fname)
     return wx.Icon(iname)
+
+def get_icon_path(fname="icon.png"):
+    dname = os.path.dirname(__file__)
+    return os.path.join(dname, fname)
 
 
 
