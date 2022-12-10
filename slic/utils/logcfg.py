412c118c773314f0f4a01ba74c02a8716c95462d
Sven Augustin <sven.augustin@psi.ch>
2022-12-10 19:06:51 +0100

    set loglevel also for logging module

--- slic/utils/logcfg.py
+++ slic/utils/logcfg.py
@@ -1,10 +1,19 @@
 import os
 import logzero
+import logging
 
 
 def logcfg(default_loglevel="WARNING"):
     loglevel = os.environ.get("LOGLEVEL", default_loglevel).upper()
-    logzero.loglevel(loglevel)
+    cfg_logzero(loglevel)
+    cfg_logging(loglevel)
+
+def cfg_logzero(level):
+    logzero.loglevel(level)
+
+def cfg_logging(level):
+    logger = logging.getLogger()
+    logger.setLevel(level)
 
 
 
