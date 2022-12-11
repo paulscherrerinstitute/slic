6a07a74c357e228526625c31bff759d646691976
Sven Augustin <sven.augustin@psi.ch>
2022-12-11 16:50:34 +0100

    added missing returns

--- slic/core/sensor/pvsensor.py
+++ slic/core/sensor/pvsensor.py
@@ -31,11 +31,13 @@ class PVSensor(Sensor):
     def start(self):
         if self._cb_index is not None:
             print("already running")
+            return
         self._cb_index = self.pv.add_callback(self._collect_cb)
 
     def stop(self):
         if self._cb_index is None:
             print("not started yet")
+            return
         self.pv.remove_callback(self._cb_index)
         self._cb_index = None
 
