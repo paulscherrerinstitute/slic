028759fa49b88b069fd17159d29419368c001cb3
Sven Augustin <sven.augustin@psi.ch>
2023-01-24 14:37:43 +0100

    added repr

--- slic/core/sensor/remoteplot.py
+++ slic/core/sensor/remoteplot.py
@@ -8,4 +8,12 @@ class RemotePlot(xrc.ServerProxy):
         super().__init__(addr, *args, **kwargs)
 
 
+    def __repr__(self):
+        orig = super().__repr__()
+        head = orig.strip("<>").rstrip("/")
+        head += " exposing:\n\n"
+        help = self.utils.help()
+        return head + help
+
+
 
