871fcf13a06231f1d2cee28e97fd1c6a73ffe5fe
Sven Augustin <sven.augustin@psi.ch>
2023-03-25 11:32:49 +0100

    expose sensor combinations

--- slic/core/sensor/__init__.py
+++ slic/core/sensor/__init__.py
@@ -1,6 +1,19 @@
 
+# single channel
 from .sensor import Sensor
 from .bssensor import BSSensor
 from .pvsensor import PVSensor
+
+# combining channels pulse by pulse
+from .bscombined import BSCombined
+from .bsnorm import BSNorm
+
+# combining sensors step by step
+from .combined import Combined
+from .norm import Norm
+
+# sensor monitoring like strip charts
+# (note that BSMonitor is the equivalent of epics monitors for BS channels)
+from .monitor import Monitor
 
 
