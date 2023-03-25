36173273a32be754470bd656e9883dd47b5fc420
Sven Augustin <sven.augustin@psi.ch>
2023-03-25 15:46:07 +0100

    changed the device structure

--- slic/devices/loptics/alvra_explaser.py
+++ slic/devices/loptics/alvra_explaser.py
@@ -13,50 +13,77 @@ class ExpLaser(Device):
         super().__init__(ID, "Experiment laser hardware", "Laser motor positions", 122)
 
         # Laser repetition rate
-        self.repRate = PVAdjustable("SIN-TIMAST-TMA:Evt-20-Freq-I")
-        self.darkRate = PVAdjustable("SIN-TIMAST-TMA:Evt-23-Freq-I")
+        self.rates = SimpleDevice("Rate",
+            rep_rate  = PVAdjustable("SIN-TIMAST-TMA:Evt-20-Freq-I"),
+            dark_rate = PVAdjustable("SIN-TIMAST-TMA:Evt-23-Freq-I")
+        )
 
         # Waveplates
-        self.wpTopas = Motor(ID + "-M442:MOT")
-        self.wpHarmonics = Motor(ID + "-M432:MOT")
+        wp_topas     = Motor(ID + "-M442:MOT")
+        wp_harmonics = Motor(ID + "-M432:MOT")
 
         # Delay stages
-        self.pumpTopas_delay = DelayStage(ID + "-M451:MOTOR_1", "Delay Pump Topas")
-        self.pumpHarmonics_delay = DelayStage(ID + "-M453:MOTOR_1", "Delay Pump Harmonics")
-        self.globalglobi_delay = DelayStage(ID + "-M452:MOTOR_1", "Delay Global Globi")
+        pump_topas  = DelayStage(ID + "-M451:MOTOR_1", "Delay Pump Topas")
+        pumpprobe   = DelayStage(ID + "-M451:MOTOR_1", "Delay Pump Probe")
+        timetool    = DelayStage(ID + "-M453:MOTOR_1", "Delay Time Tool")
+        globalglobi = DelayStage(ID + "-M452:MOTOR_1", "Delay Global Globi")
 
         # PALM delay stages
-        self.palm_delay = DelayStage(ID + "-M423:MOT")
-        self.palmEO_delay = DelayStage(ID + "-M422:MOT")
+#        palm   = DelayStage(ID + "-M423:MOT")
+#        palmEO = DelayStage(ID + "-M422:MOT")
 
         # PSEN delay stage
-        self.psen_delay = DelayStage(ID + "-M424:MOT")
+#        psen = DelayStage(ID + "-M424:MOT")
 
-        # Experimental compressor delay stage
-        self.compressorExp_delay = Motor(ID + "-M431:MOT")
-
-        # Experimental compressor delay stage
-        self.compressorDiag_delay = Motor(ID + "-M421:MOT")
+        # Compressor delay stage
+        compressor_exp  = Motor(ID + "-M431:MOT", "Compressor Experiment")
+        compressor_diag = Motor(ID + "-M421:MOT", "Compressor Diagnostics")
 
         # variable OD filter
-        self.vODfilter = Motor(ID + "-M444:MOT", "Variable OD Filter")
+        vODfilter = Motor(ID + "-M444:MOT", name="Variable OD Filter")
 
         # Experiment-FEL timing delay stage
-        self.pump_toFEL_delay = DelayStage(ID + "-M441:MOT")
+#        toFEL = DelayStage(ID + "-M441:MOT")
 
         # Experiment focussing lens position
-        self.pump_lens_focus = Motor(ID + "-M443:MOT")
+#        lens_focus = Motor(ID + "-M443:MOT")
 
         # Globi electronic timing PV from Edwin
-        self.eTiming = ETiming(ID + "-eTiming")
-        self.lxt_pp = LXTPumpProbe("SLAAR01-LTIM-PDLY:DELAYNS", pvname_done_moving="SLAAR01-LTIM-PDLY:WAITING")
+        eTiming = ETiming(ID + "-eTiming")
+        lxtpp = LXTPumpProbe("SLAAR01-LTIM-PDLY:DELAYNS", pvname_done_moving="SLAAR01-LTIM-PDLY:WAITING", name="LXT Pump Probe")
 
         # FROG
         frog_motor = SmarActAxis("SLAAR11-LMTS-FROG1")
         frog_delay = Delay(frog_motor)
-        self.FROG = SimpleDevice("FROG",
-            motor = frog_motor,
-            delay = frog_delay
+
+
+        # SimpleDevices to classify Adjustables (can probably be automatic)
+
+        self.motion = SimpleDevice("Motion",
+            wp_topas = wp_topas,
+            wp_harmonics = wp_harmonics,
+            compressor_exp = compressor_exp,
+            compressor_diag = compressor_diag,
+            vODfilter = vODfilter,
+
+            pump_topas = pump_topas.motor,
+            pumpprobe = pumpprobe.motor,
+            timetool = timetool.motor,
+            globalglobi = globalglobi.motor,
+
+            frog = frog_motor
+        )
+
+        self.delay = SimpleDevice("Delay",
+            eTiming = eTiming,
+            lxtpp = lxtpp,
+
+            pump_topas = pump_topas.delay,
+            pumpprobe = pumpprobe.delay,
+            timetool = timetool.delay,
+            globalglobi = globalglobi.delay,
+
+            frog = frog_delay
         )
 
 
