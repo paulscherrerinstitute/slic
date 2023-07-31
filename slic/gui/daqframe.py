1f09c242fe926b81d6d88c973dbedcfa6908d9ec
Sven Augustin <sven.augustin@psi.ch>
2023-07-31 19:26:52 +0200

    extracted the tab configuration into a dict

--- slic/gui/daqframe.py
+++ slic/gui/daqframe.py
@@ -9,9 +9,25 @@ from .icon import get_wx_icon
 from .persist import Persistence
 
 
+DEFAULT_TABS = {
+    "Static":  StaticPanel,
+    "Scan":    ScanPanel,
+    "Special": SpecialScanPanel, #TODO: this prohibits extracting the name from the class name
+    "Scan2D":  Scan2DPanel,
+    "Tweak":   TweakPanel,
+    "GoTo":    GoToPanel,
+    "Run":     RunPanel,
+    "SFX":     SFXPanel
+}
+
+DEFAULT_START_TAB = "Scan"
+
+
 class DAQFrame(wx.Frame):
 
-    def __init__(self, scanner, title="Neat DAQ", show_static=True, show_scan=True, show_spec=False, show_scan2D=True, show_tweak=True, show_goto=False, show_run=False, show_sfx=False):
+    def __init__(self, scanner, title="Neat DAQ", tabs=DEFAULT_TABS, start_tab=DEFAULT_START_TAB
+#        show_static=True, show_scan=True, show_spec=False, show_scan2D=True, show_tweak=True, show_goto=False, show_run=False, show_sfx=False
+    ):
         wx.Frame.__init__(self, None, title=title)#, size=(350,200))
         self.SetIcon(get_wx_icon())
 
@@ -19,33 +35,14 @@ class DAQFrame(wx.Frame):
         notebook = NotebookDX(panel_main)
         panel_main.wrap(notebook)
 
-        panel_config = cfg = ConfigPanel(notebook, scanner, name="Config")
-
-        panel_static = StaticPanel(     notebook, cfg, name="Static")
-        panel_scan   = ScanPanel(       notebook, cfg, name="Scan")
-        panel_spec   = SpecialScanPanel(notebook, cfg, name="Special")
-        panel_scan2D = Scan2DPanel(     notebook, cfg, name="Scan2D")
-        panel_tweak  = TweakPanel(      notebook, cfg, name="Tweak")
-        panel_goto   = GoToPanel(       notebook, cfg, name="GoTo")
-        panel_run    = RunPanel(        notebook, cfg, name="Run")
-        panel_sfx    = SFXPanel(        notebook, cfg, name="SFX")
-
-
+        panel_config = ConfigPanel(notebook, scanner, name="Config")
         notebook.AddPage(panel_config)
-        if show_static: notebook.AddPage(panel_static)
-        if show_scan:   notebook.AddPage(panel_scan)
-        if show_spec:   notebook.AddPage(panel_spec)
-        if show_scan2D: notebook.AddPage(panel_scan2D)
-        if show_tweak:  notebook.AddPage(panel_tweak)
-        if show_goto:   notebook.AddPage(panel_goto)
-        if show_run:    notebook.AddPage(panel_run)
-        if show_sfx:    notebook.AddPage(panel_sfx)
 
-        if   show_sfx:    notebook.SelectPage(panel_sfx)
-        elif show_spec:   notebook.SelectPage(panel_spec)
-        elif show_scan:   notebook.SelectPage(panel_scan)
-        elif show_static: notebook.SelectPage(panel_static)
-        else:             notebook.SelectPage(panel_config)
+        for name, PanelType in tabs.items():
+            p = PanelType(notebook, panel_config, name=name)
+            notebook.AddPage(p)
+
+        notebook.SelectPageByName(start_tab)
 
         # make sure the window is large enough
         sizer = wx.BoxSizer(wx.VERTICAL)
