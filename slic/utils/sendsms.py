90186443f96f687dc859507f80ae69e05dc995e5
Sven Augustin <sven.augustin@psi.ch>
2023-10-19 12:48:11 +0200

    return the message

--- slic/utils/sendsms.py
+++ slic/utils/sendsms.py
@@ -6,7 +6,7 @@ SMS_GATEWAY_ADDRESS = "swissphone-gateway.com"
 
 def sendsms(phone_number, text):
     to_addr = f"{phone_number}@{SMS_GATEWAY_ADDRESS}"
-    sendmail(to_addr, body=text)
+    return sendmail(to_addr, body=text)
 
 
 
