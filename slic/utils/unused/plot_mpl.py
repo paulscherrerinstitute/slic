5b8eeafe1101700a758f781f8012431eb752158c
Sven Augustin <sven.augustin@psi.ch>
2023-10-19 17:26:02 +0200

    removed a few clearly unused things

--- slic/utils/unused/plot_mpl.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-
-
-def identifyContrastplotData(x, y, i):
-    assert i.ndim == 2, "Intensity data needs to be 2 dimensional!"
-    assert (
-        x.ndim == y.ndim
-    ), "please provide x and y plotting coordinate in same dimension"
