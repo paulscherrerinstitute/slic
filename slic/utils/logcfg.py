6139ed9f0a6d788d3b2ee5616d1bb66746fce2c3
Sven Augustin <sven.augustin@psi.ch>
2023-12-08 12:46:54 +0100

    added TRACE log level

--- slic/utils/logcfg.py
+++ slic/utils/logcfg.py
@@ -1,6 +1,7 @@
 import os
-import logzero
 import logging
+import logzero
+from logzero import logger as log
 
 
 def logcfg(default_loglevel="WARNING"):
@@ -14,6 +15,34 @@ def cfg_logzero(level):
 def cfg_logging(level):
     logger = logging.getLogger()
     logger.setLevel(level)
+
+
+def add_log_Level(logger, level_name, level_value, func_name=None, color=None):
+    level_name = level_name.upper()
+    func_name = func_name or level_name.lower()
+
+    logging.addLevelName(level_value, level_name)
+
+    def log_func_for_class(self, *args, **kwargs):
+        self.log(level_value, *args, **kwargs)
+
+    def log_func_for_module(*args, **kwargs):
+        logging.log(level_value, *args, **kwargs)
+
+    logger_class = type(logger)
+
+    setattr(logger_class, func_name, log_func_for_class)
+    setattr(logging, func_name, log_func_for_module)
+    setattr(logging, level_name, level_value)
+
+    if color is not None:
+        color = color.upper()
+        color = getattr(logzero.ForegroundColors, color)
+        logzero.DEFAULT_COLORS[level_value] = color
+
+
+
+add_log_Level(log, "TRACE", logging.DEBUG-1, color="magenta")
 
 
 
