7f05116c67f1d3d33a2a6926dd4d8c4bf6bcf679
Sven Augustin <sven.augustin@psi.ch>
2023-12-08 13:36:01 +0100

    cleanup

--- slic/utils/screenshot.py
+++ slic/utils/screenshot.py
@@ -1,40 +1,51 @@
-import os, datetime, subprocess
-from getpass import getuser as _getuser
+import os
+import subprocess
+from datetime import datetime
+from getpass import getuser
 
 
 class Screenshot:
-    def __init__(self,screenshot_directory='',**kwargs):
-        self._screenshot_directory = screenshot_directory
-        if not ('user' in kwargs.keys()):
-            self.user=_getuser()
+
+    def __init__(self, screenshot_directory="", **kwargs):
+        self.screenshot_directory = screenshot_directory
+        if "user" not in kwargs:
+            self.user = getuser()
         else:
-            self.user = kwargs['user']
+            self.user = kwargs["user"]
+
 
     def show_directory(self):
+        cmd = ["nautilus", self.screenshot_directory]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-        p = subprocess.Popen(['nautilus',self._screenshot_directory],
-                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
-    def shoot(self,message='', window=False, desktop=False, delay=3, **kwargs):
-        cmd = ['gnome-screenshot']
+    def shoot(self, message="", window=False, desktop=False, delay=3, **kwargs):
+        cmd = ["gnome-screenshot"]
         if window:
-            cmd.append('-w')
-            cmd.append('--delay=%d'%delay)
+            cmd.append("-w")
+            cmd.append(f"--delay={delay}")
         elif desktop:
-            cmd.append('--delay=%d'%delay)
+            cmd.append(f"--delay={delay}")
         else:
-            cmd.append('-a')
-        tim = datetime.datetime.now()
-        fina = '%s-%s-%s_%s-%s-%s'%tim.timetuple()[:6]
-        if 'Author' in kwargs.keys():
-            fina+='_%s'%kwargs['Author']
+            cmd.append("-a")
+
+        ts = datetime.now().timetuple()[:6]
+        fn = "%s-%s-%s_%s-%s-%s" % ts
+
+        if "Author" in kwargs:
+            author = kwargs["Author"]
         else:
-            fina+='_%s'%self.user
-        fina+='.png'
-        filepath = os.path.join(self._screenshot_directory,fina)
-        cmd.append('--file')
+            author = self.user
+
+        fn += f"_{author}"
+        fn += ".png"
+
+        filepath = os.path.join(self.screenshot_directory, fn)
+        cmd.append("--file")
         cmd.append(filepath)
-        p = subprocess.call(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-        return filepath,p
+
+        p = subprocess.call(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return filepath, p
+
 
 
