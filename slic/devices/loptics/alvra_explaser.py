58604f8d7eee73f4515df7487e096919d23856d3
Sven Augustin <sven.augustin@psi.ch>
2023-12-11 11:21:29 +0100

    updated stages

--- slic/devices/loptics/alvra_explaser.py
+++ slic/devices/loptics/alvra_explaser.py
@@ -22,18 +22,15 @@ class ExpLaser(Device):
         wp_topas     = Motor(ID + "-M442:MOT")
         wp_harmonics = Motor(ID + "-M432:MOT")
 
-        # Delay stages
+        # Experiment table delay stages
         pump_topas  = DelayStage(ID + "-M451:MOTOR_1", "Delay Pump Topas")
         pumpprobe   = DelayStage(ID + "-M451:MOTOR_1", "Delay Pump Probe")
         timetool    = DelayStage(ID + "-M453:MOTOR_1", "Delay Time Tool")
         globalglobi = DelayStage(ID + "-M452:MOTOR_1", "Delay Global Globi")
 
-        # PALM delay stages
-#        palm   = DelayStage(ID + "-M423:MOT")
-#        palmEO = DelayStage(ID + "-M422:MOT")
-
-        # PSEN delay stage
-#        psen = DelayStage(ID + "-M424:MOT")
+        # Optical transient absorption delay stages
+        ota_long  = DelayStage(ID + "-M424:MOT",     "Delay optical transient absorption long")
+        ota_short = DelayStage(ID + "-M454:MOTOR_1", "Delay optical transient absorption short")
 
         # Compressor delay stage
         compressor_exp  = Motor(ID + "-M431:MOT", "Compressor Experiment")
@@ -42,18 +39,13 @@ class ExpLaser(Device):
         # variable OD filter
         vODfilter = Motor(ID + "-M444:MOT", name="Variable OD Filter")
 
-        # Experiment-FEL timing delay stage
-#        toFEL = DelayStage(ID + "-M441:MOT")
-
-        # Experiment focussing lens position
-#        lens_focus = Motor(ID + "-M443:MOT")
-
         # Globi electronic timing PV from Edwin
         eTiming = ETiming(ID + "-eTiming")
         lxtpp = LXTPumpProbe("SLAAR01-LTIM-PDLY:DELAYNS", pvname_done_moving="SLAAR01-LTIM-PDLY:WAITING", name="LXT Pump Probe")
+        lxtpp_ns = LXTPumpProbe("SLAAR03-LTIM-PDLY:DELAYNS_SLOW", name="OPO Pump Probe")
 
         # FROG
-        frog_motor = SmarActAxis("SLAAR11-LMTS-FROG1")
+        frog_motor = SmarActAxis("SLAAR11-LMTS-FROG11")
         frog_delay = Delay(frog_motor)
 
 
@@ -71,19 +63,26 @@ class ExpLaser(Device):
             timetool = timetool.motor,
             globalglobi = globalglobi.motor,
 
-            frog = frog_motor
+            frog = frog_motor,
+
+            ota_long = ota_long.motor,
+            ota_short = ota_short.motor
         )
 
         self.delay = SimpleDevice("Delay",
             eTiming = eTiming,
             lxtpp = lxtpp,
+            lxtpp_ns = lxtpp_ns,
 
             pump_topas = pump_topas.delay,
             pumpprobe = pumpprobe.delay,
             timetool = timetool.delay,
             globalglobi = globalglobi.delay,
 
-            frog = frog_delay
+            frog = frog_delay,
+
+            ota_long = ota_long.delay,
+            ota_short = ota_short.delay
         )
 
 
