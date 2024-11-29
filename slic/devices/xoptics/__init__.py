3c557f34f82ee475aa9f5e836f642622f14ddcf7
Sven Augustin <sven.augustin@psi.ch>
2024-11-29 15:28:11 +0100

    sorted imports

--- slic/devices/xoptics/__init__.py
+++ slic/devices/xoptics/__init__.py
@@ -3,8 +3,8 @@ from . import aramis_attenuator
 from . import aramis_reflaser
 from . import dcm
 from . import kb
+from . import offsetmirrors
 from . import pulsepicker
 from . import slits
-from . import offsetmirrors
 
 
