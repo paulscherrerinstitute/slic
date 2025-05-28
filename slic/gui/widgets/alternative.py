ec44033aad6b49e73769dd6d6588c29990940be6
Sven Augustin <sven.augustin@psi.ch>
2025-05-28 15:16:48 +0200

    fixed tooltip on entry boxes

--- slic/gui/widgets/alternative.py
+++ slic/gui/widgets/alternative.py
@@ -20,12 +20,15 @@ class Alternative(wx.BoxSizer):
     def bind_all(self, *args, **kwargs):
         for i in self.widgets:
             i.Bind(*args, **kwargs)
-            #TODO: does this make sense?
             if i.GetToolTip() is None:
                 i.SetToolTip("Right click to switch mode")
-            #TODO: is this needed?
+            # right-click event is not propagated to parent
             for j in i.GetChildren():
                 j.Bind(*args, **kwargs)
+                # avoid inheriting tooltip from parent for entry boxes without own tooltip
+                if isinstance(j, wx.TextCtrl) and j.GetToolTip() is None:
+                    suppress_tooltip(j)
+
 
     def on_right_click(self, _event):
         self.next()
@@ -53,5 +56,23 @@ class Alternative(wx.BoxSizer):
 
     def get_current(self):
         return self.widgets[self.index]
+
+
+
+def suppress_tooltip(widget):
+    """
+    if no tooltip is set, the parent tooltip is used
+    wx.ToolTip.Enable is a global setting
+    this disables/enables only within the enter/leave context
+    """
+    def on_enter(_):
+        wx.ToolTip.Enable(False)
+
+    def on_leave(_):
+        wx.ToolTip.Enable(True)
+
+    widget.Bind(wx.EVT_ENTER_WINDOW, on_enter)
+    widget.Bind(wx.EVT_LEAVE_WINDOW, on_leave)
+
 
 
