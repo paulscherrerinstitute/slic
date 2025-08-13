b11dfb4d3d5b243da275066dcedc7dc40056a10f
Sven Augustin <sven.augustin@psi.ch>
2025-08-13 20:54:31 +0200

    added get_detector_pings

--- slic/core/acquisition/broker/restapi.py
+++ slic/core/acquisition/broker/restapi.py
@@ -185,6 +185,11 @@ class BrokerSlowAPI(BaseAPI):
         response = self.get("get_jfctrl_monitor", params, *args, **kwargs)
         return response.get("parameters")
 
+    def get_detector_pings(self, detector, *args, **kwargs):
+        params = {"detector_name": detector}
+        response = self.get("get_detector_pings", params, *args, **kwargs)
+        return response.get("pings")
+
     def get_detector_status(self, detector, *args, **kwargs):
         params = {"detector_name": detector}
         response = self.get("get_detector_status", params, *args, **kwargs)
