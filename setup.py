f4492cb836201188ab0fb76a0116d06432208143
woznic_n <>
2025-08-26 13:04:53 +0200

    gitlab refs removed

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="slic",
     version='0.1.2',
-    url="https://gitlab.psi.ch/slic/slic",
+    url="https://gitea.psi.ch/slic/slic",
     description="SwissFEL Library for Instrument Control",
     author="Paul Scherrer Institute",
     packages=find_packages()
