5aa9a0a82bc9a477f910672efecc383cc891c02b
Sven Augustin <sven.augustin@psi.ch>
2025-11-16 17:34:42 +0100

    double quotes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="slic",
-    version='0.1.2',
+    version="0.1.2",
     url="https://gitea.psi.ch/slic/slic",
     description="SwissFEL Library for Instrument Control",
     author="Paul Scherrer Institute",
