4eb85719db0d9b32bc666e74c58d502526def5f9
Sven Augustin <sven.augustin@psi.ch>
2026-06-08 18:49:43 +0200

    the ambiguity was removed upstream

--- slic/devices/timing/events/ctaseq.py
+++ slic/devices/timing/events/ctaseq.py
@@ -149,8 +149,7 @@ class Config:
 
         mode = self.cta_client.StartMode(mode)
 
-        #TODO: why are modulo and divisor the same?
-        cfg = dict(modulo=divisor, offset=offset, mode=mode)
+        cfg = dict(divisor=divisor, offset=offset, mode=mode)
         self.cta_client.set_start_config(config=cfg)
 
 
